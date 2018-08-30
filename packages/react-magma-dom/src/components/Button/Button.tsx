fa410a7cb9cfe4e891293b21d0ebf20c3c5effd3
Ryan Gillett <ryan.gillett@cengage.com>
2018-08-30 15:06:34 -0400

    Covering button

--- packages/react-magma-dom/src/components/Button/Button.tsx
+++ packages/react-magma-dom/src/components/Button/Button.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 
 export interface ButtonProps {
     text: string;
+    onClick: () => void;
 }
 
-export const Button: React.SFC<ButtonProps> = ({ text }: ButtonProps): JSX.Element => (
-    <button>{text}</button>
+export const Button: React.SFC<ButtonProps> = ({ text, onClick }: ButtonProps): JSX.Element => (
+    <button onClick={onClick}>{text}</button>
 )
 
 export default Button;
