f1be15c5d969d1e3504572959a53dece8db41cf2
Ryan Gillett <ryan.gillett@cengage.com>
2018-08-30 15:31:09 -0400

    Toggle test

---
+++ packages/react-magma-dom/src/components/Toggle/Toggle.test.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Toggle, ToggleProps } from './Toggle';
+import { render, fireEvent, cleanup, waitForElement } from 'react-testing-library'
+
+const BASE_TOGGLE_PROPS: ToggleProps = {
+    handleToggle: jest.fn(),
+    isOn: false
+};
+
+const renderButton = (myProps = {}) => {
+    const props = {
+        ...BASE_TOGGLE_PROPS,
+        ...myProps
+    };
+
+    return render(<Toggle {...props} />);
+}
+
+describe('Toggle', () => {
+
+    afterEach(() => {
+        cleanup();
+    });
+
+    it('should render a button with the passed in text', () => {
+        const { debug } = renderButton();
+
+        debug();
+
+        expect(true).toBeTruthy();
+    });
+})
