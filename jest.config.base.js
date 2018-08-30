39187a99985987b402e189ddfa40b0c70c88dab5
Van Slaars, Andy L <andy.vanslaars@cengage.com>
2018-08-30 13:25:35 -0400

    Got tests building and running

--- jest.config.base.js
+++ jest.config.base.js
@@ -1,21 +1,10 @@
 module.exports = {
-    testMatch: [
-        "<rootDir>/src/**/?(*.)(spec|test).{ts,tsx,mjs}"
-    ],
-    transform: {
-        "^.+\\.ts$": "ts-jest"
-    },
-    moduleFileExtensions: [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    collectCoverage: true,
-    coveragePathIgnorePatterns: [
-        "(tests/.*.mock).(jsx?|tsx?)$"
-    ],
-    verbose: true
-};
+  testMatch: ['<rootDir>/src/**/?(*.)(spec|test).{ts,tsx,mjs}'],
+  transform: {
+    '^.+\\.ts(x)?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverage: true,
+  coveragePathIgnorePatterns: ['(tests/.*.mock).(jsx?|tsx?)$'],
+  verbose: true
+}
