2688b4b65d7d850769fd43c4c7e299d1fa395155
Ryan Gillett <ryan.gillett@cengage.com>
2018-09-13 16:03:58 -0400

    Initial native input

---
+++ packages/react-magma-native/src/components/Input/Input.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import { InputCore } from 'react-magma-core';
+
+export interface InputProps {
+  autoFocus?: boolean;
+  placeholder?: string;
+  value?: string;
+}
+
+export const Input: React.SFC<InputProps> = (
+  props: InputProps
+): JSX.Element => (
+  <InputCore value={props.value}>
+    {({ handleBlur, handleChange, handleFocus, value }) => {
+      const { autoFocus, placeholder } = props;
+
+      return (
+        <View>
+          <TextInput
+            autoFocus={autoFocus}
+            placeholder={placeholder}
+            value={value}
+            onBlur={handleBlur}
+            onChange={handleChange}
+            onFocus={handleFocus}
+          />
+        </View>
+      );
+    }}
+  </InputCore>
+);
+
+export default Input;
