7d583ca12e6486ce2e23708701348f0e5bced510
Ryan Gillett <ryan.gillett@cengage.com>
2018-09-13 13:10:14 -0400

    Husky

--- packages/react-magma-native/src/components/Toggle/Toggle.tsx
+++ packages/react-magma-native/src/components/Toggle/Toggle.tsx
@@ -1,35 +1,34 @@
-import * as React from 'react'
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
-import { ToggleCore } from 'react-magma-core'
+import * as React from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { ToggleCore } from 'react-magma-core';
 
 export interface ToggleProps {
-    isOn?: boolean,
-    handleToggle?: () => void
+  isOn?: boolean;
+  handleToggle?: () => void;
 }
 
-export const Toggle: React.SFC<ToggleProps> = (props: ToggleProps): JSX.Element => (
-    <ToggleCore>
-        {({ isOn, handleToggle }) => {
-          return (
-            <View>
-              <TouchableOpacity
-                onPress={handleToggle}
-                style={styles.toggleView}
-              >
-                <Text>{isOn ? 'On' : 'Off Blah'}</Text>
-              </TouchableOpacity>
-            </View>
-          )
-        }}
-    </ToggleCore>
-)
+export const Toggle: React.SFC<ToggleProps> = (
+  props: ToggleProps
+): JSX.Element => (
+  <ToggleCore>
+    {({ isOn, handleToggle }) => {
+      return (
+        <View>
+          <TouchableOpacity onPress={handleToggle} style={styles.toggleView}>
+            <Text>{isOn ? 'On' : 'Off Blah'}</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    }}
+  </ToggleCore>
+);
 
 const styles = StyleSheet.create({
-    toggleView: {
-        width: 200,
-        height: 50,
-        backgroundColor: '#ccc'
-    }
+  toggleView: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#ccc'
+  }
 });
 
 export default Toggle;
