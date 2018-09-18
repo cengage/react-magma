151850c0d923c13bc3a40f40a1df624d0ba8a894
Ryan Gillett <ryan.gillett@cengage.com>
2018-09-18 15:00:53 -0400

    Changing button props

--- packages/react-magma-dom/src/components/Button/Button.tsx
+++ packages/react-magma-dom/src/components/Button/Button.tsx
@@ -2,83 +2,91 @@ import * as React from 'react';
 const styled = require('styled-components').default;
 import { magma } from '../../theme/magma';
 
+enum ButtonType {
+  default,
+  primary,
+  success,
+  warning,
+  danger,
+  link
+}
+
+enum ButtonSize {
+  large,
+  small
+}
+
 export interface ButtonProps {
   text: string;
   onClick: () => void;
   disabled?: boolean;
   ghost?: boolean;
-  link?: boolean;
-
-  // button sizes
-  large?: boolean;
-  small?: boolean;
-
-  // button styles
-  default?: boolean;
-  primary?: boolean;
-  success?: boolean;
-  warning?: boolean;
-  danger?: boolean;
+  size?: ButtonSize;
+  type?: ButtonType;
 }
 
 const StyledButton = styled.button`
   background-color: ${props => {
-    if (props.ghost || props.link) {
+    if (props.ghost) {
       return 'none';
     }
-    if (props.primary) {
-      return magma.primary02;
+
+    switch (props.type) {
+      case 'link':
+        return 'none';
+      case 'primary':
+        return magma.primary02;
+      case 'success':
+        return magma.accent02;
+      case 'warning':
+        return magma.accent05;
+      case 'danger':
+        return magma.limited01;
+      default:
+        return magma.primary04;
     }
-    if (props.success) {
-      return magma.accent02;
-    }
-    if (props.warning) {
-      return magma.accent05;
-    }
-    if (props.danger) {
-      return magma.limited01;
-    }
-    return magma.primary04;
   }};
   border: 1px solid transparent;
   border-color: ${props => {
-    if (props.link) {
-      return 'transparent';
+    switch (props.type) {
+      case 'link':
+        return 'transparent';
+      case 'primary':
+        return magma.primary02;
+      case 'success':
+        return magma.accent02;
+      case 'warning':
+        return magma.accent05;
+      case 'danger':
+        return magma.limited01;
+      default:
+        return magma.secondary05;
     }
-    if (props.primary) {
-      return magma.primary02;
-    }
-    if (props.success) {
-      return magma.accent02;
-    }
-    if (props.warning) {
-      return magma.accent05;
-    }
-    if (props.danger) {
-      return magma.limited01;
-    }
-    return magma.secondary05;
   }};
   color: ${props => {
-    if (props.link) {
+    if (props.type === 'link') {
       return magma.primary02;
     }
     if (props.ghost) {
-      if (props.primary) {
-        return magma.primary02;
+      switch (props.type) {
+        case 'primary':
+          return magma.primary02;
+        case 'success':
+          return magma.accent02;
+        case 'warning':
+          return magma.accent05;
+        case 'danger':
+          return magma.limited01;
+        default:
+          return magma.secondary04;
       }
-      if (props.success) {
-        return magma.accent02;
-      }
-      if (props.warning) {
-        return magma.accent05;
-      }
-      if (props.danger) {
-        return magma.limited01;
-      }
-      return magma.secondary04;
     }
-    if (props.primary || props.success || props.warning || props.danger) {
+    if (
+      props.type === 'primary' ||
+      props.type === 'success' ||
+      props.type === 'warning' ||
+      props.type === 'danger'
+    ) {
       return magma.primary04;
     }
     return magma.accent02;
@@ -88,24 +96,26 @@ const StyledButton = styled.button`
   display: inline-block;
   font-family: ${magma.bodyFont};
   font-size: ${props => {
-    if (props.large) {
-      return '20px';
+    switch (props.size) {
+      case 'large':
+        return '20px';
+      case 'small':
+        return '14px';
+      default:
+        return '16px';
     }
-    if (props.small) {
-      return '14px';
-    }
-    return '16px';
   }};
   line-height: 1.46666667;
   margin: 5px;
   padding: ${props => {
-    if (props.large) {
-      return '0.4em 20px';
+    switch (props.size) {
+      case 'large':
+        return '0.4em 20px';
+      case 'small':
+        return '0.4em 10px';
+      default:
+        return '0.4em 15px';
     }
-    if (props.small) {
-      return '0.4em 10px';
-    }
-    return '0.4em 15px';
   }};
   text-align: center;
   vertical-align: middle;
@@ -115,90 +125,87 @@ const StyledButton = styled.button`
   &:hover:not([disabled]),
   &:focus:not([disabled]) {
     background-color: ${props => {
-      if (props.primary) {
-        return magma.secondary02;
+      switch (props.type) {
+        case 'primary':
+          return magma.secondary02;
+        case 'success':
+          return magma.accent03;
+        case 'warning':
+          return magma.accent06;
+        case 'danger':
+          return magma.limited02;
+        default:
+          return magma.primary04;
       }
-      if (props.success) {
-        return magma.accent03;
-      }
-      if (props.warning) {
-        return magma.accent06;
-      }
-      if (props.danger) {
-        return magma.limited02;
-      }
-      return magma.primary04;
     }};
     border-color: ${props => {
-      if (props.link) {
-        return 'transparent';
+      switch (props.type) {
+        case 'link':
+          return 'transparent';
+        case 'primary':
+          return magma.secondary01;
+        case 'success':
+          return magma.accent03;
+        case 'warning':
+          return magma.accent06;
+        case 'danger':
+          return magma.limited02;
+        default:
+          return magma.secondary05;
       }
-      if (props.primary) {
-        return magma.secondary02;
-      }
-      if (props.success) {
-        return magma.accent03;
-      }
-      if (props.warning) {
-        return magma.accent06;
-      }
-      if (props.danger) {
-        return magma.limited02;
-      }
-      return magma.secondary05;
     }};
     color: ${props => {
-      if (props.link) {
-        return magma.secondary01;
+      switch (props.type) {
+        case 'link':
+          return magma.secondary01;
+        case 'primary':
+        case 'success':
+        case 'warning':
+        case 'danger':
+          return magma.primary04;
+        default:
+          return magma.accent01;
       }
-      if (props.primary || props.success || props.warning || props.danger) {
-        return magma.primary04;
-      }
-      return magma.accent01;
     }};
-    text-decoration: ${props => (props.link ? 'underline' : 'none')};
+    text-decoration: ${props => (props.type === 'link' ? 'underline' : 'none')};
   }
 
   &:active:not([disabled]),
   &.active:not([disabled]) {
     background-color: ${props => {
-      if (props.link) {
-        return 'transparent';
+      switch (props.type) {
+        case 'link':
+          return 'transparent';
+        case 'primary':
+          return magma.secondary01;
+        case 'success':
+          return magma.accent01;
+        case 'warning':
+          return magma.accent04;
+        case 'danger':
+          return magma.limited04;
+        default:
+          return magma.secondary06;
       }
-      if (props.primary) {
-        return magma.secondary01;
-      }
-      if (props.success) {
-        return magma.accent01;
-      }
-      if (props.warning) {
-        return magma.accent04;
-      }
-      if (props.danger) {
-        return magma.limited04;
-      }
-      return magma.secondary06;
     }};
     border-color: ${props => {
-      if (props.link) {
-        return 'transparent';
+      switch (props.type) {
+        case 'link':
+          return 'transparent';
+        case 'primary':
+          return magma.secondary01;
+        case 'success':
+          return magma.accent01;
+        case 'warning':
+          return magma.accent04;
+        case 'danger':
+          return magma.limited04;
+        default:
+          return magma.secondary05;
       }
-      if (props.primary) {
-        return magma.secondary01;
-      }
-      if (props.success) {
-        return magma.accent01;
-      }
-      if (props.warning) {
-        return magma.accent04;
-      }
-      if (props.danger) {
-        return magma.limited04;
-      }
-      return magma.secondary05;
     }};
     box-shadow: ${props =>
-      props.link ? '0 0 0' : 'inset 0 3px 5px rgba(0, 0, 0, 0.125)'};
+      props.type === 'link' ? '0 0 0' : 'inset 0 3px 5px rgba(0, 0, 0, 0.125)'};
   }
 
   &[disabled] {
@@ -215,26 +222,16 @@ export const Button: React.SFC<ButtonProps> = ({
   text,
   onClick,
   ghost,
-  link,
   disabled,
-  large,
-  small,
-  primary,
-  success,
-  warning,
-  danger
+  size,
+  type
 }: ButtonProps): JSX.Element => (
   <StyledButton
     onClick={onClick}
     disabled={disabled}
-    large={large}
-    small={small}
     ghost={ghost}
-    link={link}
-    primary={primary}
-    success={success}
-    warning={warning}
-    danger={danger}
+    size={size}
+    type={type}
   >
     {text}
   </StyledButton>
