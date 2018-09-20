f644aad263d125aef284c589427333acbaa784a9
Van Slaars, Andy L <andy.vanslaars@cengage.com>
2018-09-20 16:31:42 -0400

    chore: added Jest watch typeahead plugin and root-level test-watch script

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
-const base = require("./jest.config.base.js");
+const base = require('./jest.config.base.js');
 
 module.exports = {
-    ...base,
-    projects:
-    [
-        "<rootDir>/packages/*/jest.config.js"
-    ],
-    coverageDirectory: "<rootDir>/coverage/"
+  ...base,
+  projects: [
+    '<rootDir>/packages/react-magma-core/jest.config.js',
+    '<rootDir>/packages/react-magma-dom/jest.config.js'
+  ],
+  coverageDirectory: '<rootDir>/coverage/'
 };
