744f93f5599cc9a416232c7478a501624734d51d
Smith, Joanne <joanne.smith@cengage.com>
2018-10-05 14:18:06 -0500

    remove broken export

--- packages/react-magma-dom/src/index.ts
+++ packages/react-magma-dom/src/index.ts
@@ -1,6 +1,5 @@
 export { Button } from './components/Button/Button';
 export { Input } from './components/Input/Input';
 export { magma } from './theme/magma';
-export { globalStyle } from './theme/globalStyle';
 
 import './theme/globalStyle';
