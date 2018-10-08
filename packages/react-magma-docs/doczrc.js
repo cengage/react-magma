33ce0ce69e3cb9294bff0fec8be7b82f8f7840d5
Smith, Joanne <joanne.smith@cengage.com>
2018-10-08 14:55:09 -0500

    use magma colors in docz theme

--- packages/react-magma-docs/doczrc.js
+++ packages/react-magma-docs/doczrc.js
@@ -1,13 +1,19 @@
 import { reactNative } from 'docz-plugin-react-native';
 import magma from 'react-magma-dom';
 
+const theme = magma.magma;
+
 export default {
   title: 'React Magma',
   typescript: true,
   plugins: [reactNative()],
   themeConfig: {
     colors: {
-      primary: '#007db8',
+      primary: theme.primary02,
+      link: theme.primary02,
+      sidebarBg: theme.primary03,
+      sidebarText: theme.primary01,
+      text: theme.primary01,
     },
     logo: {
       src: '/static/img/React_Magma_Logo_Full.f03d55c8.png',
@@ -15,24 +21,24 @@ export default {
     },
     styles: {
       h1: {
-        color: '#007db8',
-        fontFamily: 'gnuolane, sans-serif',
+        color: theme.primary02,
+        fontFamily: theme.headingFont,
         fontSize: '40px',
         fontWeight: 500,
         lineHeight: 1.1,
         margin: '20px 0 10px'
       },
       h2: {
-        color: '#007db8',
-        fontFamily: 'gnuolane, sans-serif',
+        color: theme.primary02,
+        fontFamily: theme.headingFont,
         fontSize: '32px',
         fontWeight: 500,
         lineHeight: 1.1,
         margin: '20px 0 10px'
       },
       h3: {
-        color: '#007db8',
-        fontFamily: 'gnuolane, sans-serif',
+        color: theme.primary02,
+        fontFamily: theme.headingFont,
         fontSize: '26px',
         fontWeight: 500,
         lineHeight: 1.1,
