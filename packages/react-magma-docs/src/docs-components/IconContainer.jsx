4b5353a8d21f9b98a41125d32788d8b69b2f42d5
Ryan Gillett <ryan.gillett@cengage.com>
2018-10-18 16:14:01 -0400

    docs(icons): making border on icon container optional

--- packages/react-magma-docs/src/docs-components/IconContainer.jsx
+++ packages/react-magma-docs/src/docs-components/IconContainer.jsx
@@ -4,9 +4,11 @@ const styled = require('styled-components').default;
 const StyledSpan = styled.span`
   padding: 20px;
   text-align: center;
-  border: 1px solid #ccc;
+  border: ${({ border }) => (border ? '1px solid #ccc' : null)}
   margin: 10px;
   min-width: 200px;
 `;
 
-export const IconContainer = props => <StyledSpan>{props.children}</StyledSpan>;
+export const IconContainer = props => (
+  <StyledSpan border={props.border}>{props.children}</StyledSpan>
+);
