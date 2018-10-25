161a66c3b9ce5b0377a609f2e6813a318ebd9b00
Van Slaars, Andy L <andy.vanslaars@cengage.com>
2018-10-25 16:10:03 -0400

    docs(gatsby): Added styled-components plugin

--- packages/react-magma-docs-gatsby/gatsby-config.js
+++ packages/react-magma-docs-gatsby/gatsby-config.js
@@ -5,6 +5,12 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     {
+      resolve: 'gatsby-plugin-styled-components',
+      options: {
+        transpileTemplateLiterals: true,
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
