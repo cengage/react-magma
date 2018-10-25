dba5019025aa74385cc886c5527aedc7ef605640
Van Slaars, Andy L <andy.vanslaars@cengage.com>
2018-10-25 15:57:06 -0400

    docs(gatsby): added titles and cleaned up gatsby-node

--- packages/react-magma-docs-gatsby/gatsby-node.js
+++ packages/react-magma-docs-gatsby/gatsby-node.js
@@ -1,44 +1,5 @@
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
-const componentWithMDXScope = require('gatsby-mdx/component-with-mdx-scope')
-
-createPages = ({ graphql, actions }) => {
-  const { createPage } = actions
-  return new Promise((resolve, reject) => {
-    graphql(`
-      {
-        allMdx {
-          edges {
-            node {
-              fields {
-                slug
-              }
-              code {
-                scope
-              }
-            }
-          }
-        }
-      }
-    `).then(result => {
-      if (result.errors) {
-        reject(result.errors)
-      }
-      result.data.allMdx.edges.forEach(({ node }) => {
-        createPage({
-          path: node.fields.slug,
-          component: componentWithMDXScope(
-            path.resolve('./src/templates/pageTemplate.js'),
-            node.code.scope,
-            __dirname
-          ),
-          context: { id: node.id, slug: node.fields.slug },
-        })
-      })
-    })
-    resolve()
-  })
-}
 
 const getPathPrefix = path => (/design/.test(path) ? 'design' : 'api')
 
