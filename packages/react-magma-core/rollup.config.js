07970cfdbf1679de4ee486675ed46452d51d4966
Ryan Gillett <ryan.gillett@cengage.com>
2018-11-29 10:52:11 -0500

    build(react-magma-core): updating build to use babel typescript and tsc

--- packages/react-magma-core/rollup.config.js
+++ packages/react-magma-core/rollup.config.js
@@ -1,32 +1,53 @@
-import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import pkg from './package.json';
+
+const base = {
+  input: 'src/index.ts',
+  external: ['react', 'styled-components', 'react-select'],
+  plugins: [
+    resolve({
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.ts', '.tsx']
+    }),
+    terser()
+  ]
+};
 
 export default [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/bundle.js',
-        format: 'cjs'
-      },
-      {
-        file: 'dist/esBundle.js',
-        format: 'esm'
-      },
-      {
-        name: 'umdBundle',
-        file: 'dist/umdBundle.js',
-        format: 'umd'
+    ...base,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      globals: {
+        react: 'React'
       }
-    ],
-    external: ['react'],
-    plugins: [
-      typescript(),
-      resolve(),
-      babel({
-        exclude: 'node_modules/**'
-      })
-    ]
+    }
+  },
+  {
+    ...base,
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      globals: {
+        react: 'React'
+      }
+    }
+  },
+  {
+    ...base,
+    output: {
+      name: 'umdBundle',
+      file: 'dist/umdBundle.js',
+      format: 'umd',
+      globals: {
+        react: 'React'
+      }
+    }
   }
 ];
