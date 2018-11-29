495a224337c898b36476b156406eff1b93c5bd18
Ryan Gillett <ryan.gillett@cengage.com>
2018-11-29 10:06:01 -0500

    build(react-magma-dom): Building with babel typescript

--- packages/react-magma-dom/rollup.config.js
+++ packages/react-magma-dom/rollup.config.js
@@ -1,38 +1,65 @@
-import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
 import url from 'postcss-url';
+import pkg from './package.json';
+
+const base = {
+  input: 'src/index.ts',
+  external: ['react', 'styled-components', 'react-select'],
+  plugins: [
+    postcss({
+      extensions: ['.css'],
+      plugins: [url]
+    }),
+    resolve({
+      extensions: ['.ts', '.tsx', '.js', '.json']
+    }),
+    babel({
+      exclude: 'node_modules/**',
+      extensions: ['.ts', '.tsx']
+    }),
+    terser()
+  ]
+};
 
 export default [
   {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: 'dist/bundle.js',
-        format: 'cjs'
-      },
-      {
-        file: 'dist/esBundle.js',
-        format: 'esm'
-      },
-      {
-        name: 'umdBundle',
-        file: 'dist/umdBundle.js',
-        format: 'umd'
+    ...base,
+    output: {
+      file: pkg.main,
+      format: 'cjs',
+      globals: {
+        react: 'React',
+        'styled-components': 'styled',
+        'react-select': 'ReactSelect'
       }
-    ],
-    external: ['react', 'styled-components', 'react-select'],
-    plugins: [
-      postcss({
-        extensions: ['.css'],
-        plugins: [url]
-      }),
-      resolve(),
-      typescript(),
-      babel({
-        exclude: 'node_modules/**'
-      })
-    ]
+    }
+  },
+  {
+    ...base,
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      globals: {
+        react: 'React',
+        'styled-components': 'styled',
+        'react-select': 'ReactSelect'
+      }
+    }
+  },
+  {
+    ...base,
+    output: {
+      name: 'umdBundle',
+      file: 'dist/umdBundle.js',
+      format: 'umd',
+      globals: {
+        react: 'React',
+        'styled-components': 'styled',
+        'react-select': 'ReactSelect'
+      }
+    }
   }
 ];
