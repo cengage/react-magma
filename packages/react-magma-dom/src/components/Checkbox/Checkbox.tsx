d79907897af605db314e25ab7604b2d0d751d3a9
Smith, Joanne <joanne.smith@cengage.com>
2018-11-30 09:38:38 -0600

    style(checkboxes): checkbox styles
    MAGMA-77

--- packages/react-magma-dom/src/components/Checkbox/Checkbox.tsx
+++ packages/react-magma-dom/src/components/Checkbox/Checkbox.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { CheckboxCore } from 'react-magma-core';
+import { Icon } from '../Icon/Icon';
 const styled = require('styled-components').default;
 import { magma } from '../../theme/magma';
 
@@ -16,6 +17,10 @@ export interface CheckboxProps {
   value?: string;
 }
 
+export interface CheckboxWrapperProps {
+  checked?: boolean;
+}
+
 const StyledContainer = styled.div`
   align-items: baseline;
   display: ${props => (props.inline ? 'inline-flex' : 'flex')};
@@ -23,13 +28,54 @@ const StyledContainer = styled.div`
   margin: 0 0 5px;
 `;
 
-const StyledInput = styled.input``;
+const StyledInput = styled.input`
+  clip: rect(1px, 1px, 1px, 1px);
+  height: 1px;
+  position: absolute;
+  overflow: hidden;
+  top: auto;
+  white-space: nowrap;
+  width: 1px;
 
-const StyledLabel = styled.label`
-  margin: ${props => (props.inline ? '0 20px 0 10px' : '0 0 0 10px')};
+  + label svg {
+    display: block;
+    fill: #fff;
+    opacity: 0;
+    transition: all 0.1s ease-out;
+  }
+
+  &:checked + label svg {
+    opacity: 1;
+  }
 `;
 
-export const Checkbox: React.SFC<CheckboxProps> = (
+const StyledLabel = styled.label`
+  margin: ${props => (props.inline ? '0 35px 0 10px' : '0 0 0 10px')};
+`;
+
+const StyledSpan = styled.span<CheckboxWrapperProps>`
+  align-items: center;
+  background: ${props => (props.checked ? '#006298' : '#fff')};
+  border: 2px solid #006298;
+  border-radius: 3px;
+  cursor: pointer;
+  display: flex;
+  float: left;
+  height: 20px;
+  justify-content: center;
+  margin: 2px 5px 0 -25px;
+  transition: all 0.2s ease-out;
+  width: 20px;
+`;
+
+const StyledIcon = styled(Icon)<CheckboxWrapperProps>`
+  display: block;
+  fill: #fff;
+  opacity: ${props => (props.checked ? '1' : '0')};
+  transition: all 0.2s ease-out;
+`;
+
+export const Checkbox: React.FunctionComponent<CheckboxProps> = (
   props: CheckboxProps
 ): JSX.Element => (
   <CheckboxCore
@@ -39,7 +85,7 @@ export const Checkbox: React.SFC<CheckboxProps> = (
     handleFocus={props.handleFocus}
   >
     {({ handleBlur, handleChange, handleFocus, value }) => {
-      const { autoFocus, id, inline, disabled, labelText, required } = props;
+      const { autoFocus, disabled, id, inline, labelText, required } = props;
 
       return (
         <StyledContainer inline={inline}>
@@ -55,6 +101,9 @@ export const Checkbox: React.SFC<CheckboxProps> = (
             onFocus={handleFocus}
           />
           <StyledLabel htmlFor={id} inline={inline}>
+            <StyledSpan checked={value}>
+              <StyledIcon size={12} type="checkmark" />
+            </StyledSpan>
             {labelText}
           </StyledLabel>
         </StyledContainer>
