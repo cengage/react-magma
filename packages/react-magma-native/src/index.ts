52caa0768672a3e5c5c68aa88e79c8d5e87baed9
Smith, Joanne <joanne.smith@cengage.com>
2019-01-18 13:24:22 -0600

    feat(toggle): remove toggle from react-magma-core and react-magma-native

--- packages/react-magma-native/src/index.ts
+++
@@ -1,1 +0,0 @@
-export { Toggle } from './components/Toggle/Toggle';
--- packages/testNativeApp/App.js
+++ packages/testNativeApp/App.js
@@ -8,7 +8,6 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Toggle } from 'react-magma-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -25,7 +24,6 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
-        <Toggle />
       </View>
     );
   }
