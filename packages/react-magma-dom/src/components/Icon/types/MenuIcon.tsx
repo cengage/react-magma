b65a9c37bd5e2751e30f97a28f15920796cba135
Smith, Joanne <joanne.smith@cengage.com>
2019-03-15 14:45:25 -0500

    feat(icon): update menu icon

--- packages/react-magma-dom/src/components/Icon/types/MenuIcon.tsx
+++ packages/react-magma-dom/src/components/Icon/types/MenuIcon.tsx
@@ -2,11 +2,12 @@ import * as React from 'react';
 import { IconProps, renderIcon } from '../utils';
 
 const iconType = {
-  viewBox: '0 0 24.5 19.25',
+  viewBox: '0 0 20 14',
   paths: [
     {
-      d: 'M1.75,4.38h24.5V9.62H1.75Zm0,7h24.5v5.24H1.75Zm0,7h24.5v5.24H1.75Z',
-      transform: 'translate(-1.75 -4.38)'
+      d:
+        'M0.5,16.165 L19.4974999,16.165 L19.4974999,14.0541666 L0.5,14.0541666 L0.5,16.165 Z M0.5,10.8879166 L19.4974999,10.8879166 L19.4974999,8.77708332 L0.5,8.77708332 L0.5,10.8879166 Z M0.5,3.5 L0.5,5.61083333 L19.4974999,5.61083333 L19.4974999,3.5 L0.5,3.5 Z',
+      transform: 'translate(0 -3)'
     }
   ]
 };
