5ddf37b3d9867887d2776ae3e39662155a4f6f1c
Smith, Joanne <joanne.smith@cengage.com>
2019-04-19 08:45:56 -0500

    test(global styles): update test

--- packages/react-magma-dom/src/theme/GlobalStyles.test.js
+++ packages/react-magma-dom/src/theme/GlobalStyles.test.js
@@ -2,17 +2,13 @@ import React from 'react';
 import { GlobalStyles } from './GlobalStyles';
 import { render, cleanup } from 'react-testing-library';
 
-const renderGlobalStyles = () => {
-  return render(<GlobalStyles />);
-};
-
 describe('GlobalStyles', () => {
   afterEach(() => {
     cleanup();
   });
 
   it('should render the global styles', () => {
-    const { container } = renderGlobalStyles();
+    const { container } = render(<GlobalStyles />);
 
     expect(container).toBeInTheDocument();
   });
