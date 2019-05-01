116904847ba698505db6cde0face806abd624c2c
Ryan Gillett <ryan.gillett@cengage.com>
2019-05-01 14:46:54 -0400

    fix(lint-fixes): adding eslint to core for tests

---
+++ packages/react-magma-core/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    jest: true
+  },
+  extends: 'cengage/react',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react'],
+  rules: {
+    'react/display-name': 0
+  }
+};
