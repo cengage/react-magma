b4c39b61dd31ea806badaec38e82a99f834e312d
Ryan Gillett <ryan.gillett@cengage.com>
2019-05-07 14:41:14 -0400

    refactor(button-core): remove button core

--- packages/react-magma-core/src/index.ts
+++ packages/react-magma-core/src/index.ts
@@ -1,5 +1,4 @@
 export { InputCore } from './components/Input/Input';
-export { ButtonCore } from './components/Button/Button';
 export { CheckboxCore } from './components/Checkbox/Checkbox';
 export { RadioCore } from './components/Radio/Radio';
 export { SelectCore } from './components/Select/Select';
