3969d2d980876baee02aa1ca3af7cef002256758
Smith, Joanne <joanne.smith@cengage.com>
2019-06-07 15:09:26 -0500

    feat(datepicker): help button style

--- packages/react-magma-dom/src/components/DatePicker/CalendarMonth.tsx
+++ packages/react-magma-dom/src/components/DatePicker/CalendarMonth.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
+import { Button } from '../Button';
+import { ButtonVariant } from '../StyledButton';
+import { QuestionCircleOIcon } from '../Icon/types/QuestionCircleOIcon';
 import { CalendarContext } from './CalendarContext';
 import { CalendarHeader } from './CalendarHeader';
 import { CalendarDay } from './CalendarDay';
-import { magma } from '../../theme/magma';
+import { ThemeContext } from '../../theme/themeContext';
 import styled from '@emotion/styled';
 import { format } from 'date-fns';
 import { HelperInformation } from './HelperInformation';
@@ -13,12 +16,12 @@ interface CalendarMonthState {
 }
 
 const CalendarContainer = styled.div`
-  background: ${magma.colors.neutral08};
+  background: ${props => props.theme.colors.neutral08};
   padding: 0 19px 10px;
 `;
 
 const MonthContainer = styled.div`
-  background: ${magma.colors.neutral08};
+  background: ${props => props.theme.colors.neutral08};
   text-align: center;
   user-select: none;
   vertical-align: top;
@@ -31,30 +34,18 @@ const Table = styled.table`
 
 const Th = styled.th`
   border: 0;
-  color: ${magma.colors.neutral02};
+  color: $ ${props => props.theme.colors.neutral02};
   font-size: 13px;
   font-weight: normal;
   padding: 0;
   text-align: center;
 `;
 
-const HelperButton = styled.button`
-  color: inherit;
-  cursor: pointer;
-  width: 33px;
-  height: 26px;
-  position: absolute;
-  z-index: 2;
+const HelperButton = styled.span`
   bottom: 0px;
+  position: absolute;
   right: 0px;
-  background: none;
-  border-width: 0px;
-  border-style: initial;
-  border-color: initial;
-  border-image: initial;
-  border-radius: 0px;
-  overflow: visible;
-  padding: 0px;
+  z-index: 2;
 `;
 
 export class CalendarMonth extends React.Component<{}, CalendarMonthState> {
@@ -85,58 +76,65 @@ export class CalendarMonth extends React.Component<{}, CalendarMonthState> {
 
   render() {
     return (
-      <CalendarContext.Consumer>
-        {context =>
-          context && (
-            <CalendarContainer>
-              <MonthContainer data-visible="true">
-                <CalendarHeader
-                  text={format(context.focusedDate, 'MMMM YYYY')}
-                />
-                <Table
-                  role="presentation"
-                  onKeyDown={context.onKeyDown}
-                  onBlur={this.onCalendarTableBlur}
-                  onFocus={this.onCalendarTableFocus}
-                >
-                  <tbody>
-                    <tr>
-                      <Th>S</Th>
-                      <Th>M</Th>
-                      <Th>T</Th>
-                      <Th>W</Th>
-                      <Th>T</Th>
-                      <Th>F</Th>
-                      <Th>S</Th>
-                    </tr>
-                    {context
-                      .buildCalendarMonth(context.focusedDate)
-                      .map((week, i) => (
-                        <tr key={i}>
-                          {week.map((day, dayOfWeek) => (
-                            <CalendarDay
-                              key={dayOfWeek}
-                              day={day}
-                              dayFocusable={this.state.dayFocusable}
-                              onDayClick={context.onDayClick}
-                            />
-                          ))}
+      <ThemeContext.Consumer>
+        {theme => (
+          <CalendarContext.Consumer>
+            {context =>
+              context && (
+                <CalendarContainer theme={theme}>
+                  <MonthContainer data-visible="true" theme={theme}>
+                    <CalendarHeader
+                      text={format(context.focusedDate, 'MMMM YYYY')}
+                    />
+                    <Table
+                      role="presentation"
+                      onKeyDown={context.onKeyDown}
+                      onBlur={this.onCalendarTableBlur}
+                      onFocus={this.onCalendarTableFocus}
+                    >
+                      <tbody>
+                        <tr>
+                          <Th theme={theme}>S</Th>
+                          <Th theme={theme}>M</Th>
+                          <Th theme={theme}>T</Th>
+                          <Th theme={theme}>W</Th>
+                          <Th theme={theme}>T</Th>
+                          <Th theme={theme}>F</Th>
+                          <Th theme={theme}>S</Th>
                         </tr>
-                      ))}
-                  </tbody>
-                </Table>
-                <HelperButton
-                  onClick={this.onHelperButtonClick}
-                  onFocus={context.onHelperFocus}
-                >
-                  ?
-                </HelperButton>
-                {this.state.showHelperInformation && <HelperInformation />}
-              </MonthContainer>
-            </CalendarContainer>
-          )
-        }
-      </CalendarContext.Consumer>
+                        {context
+                          .buildCalendarMonth(context.focusedDate)
+                          .map((week, i) => (
+                            <tr key={i}>
+                              {week.map((day, dayOfWeek) => (
+                                <CalendarDay
+                                  key={dayOfWeek}
+                                  day={day}
+                                  dayFocusable={this.state.dayFocusable}
+                                  onDayClick={context.onDayClick}
+                                />
+                              ))}
+                            </tr>
+                          ))}
+                      </tbody>
+                    </Table>
+                    <HelperButton>
+                      <Button
+                        ariaLabel="Help"
+                        icon={<QuestionCircleOIcon />}
+                        onClick={this.onHelperButtonClick}
+                        onFocus={context.onHelperFocus}
+                        variant={ButtonVariant.link}
+                      />
+                    </HelperButton>
+                    {this.state.showHelperInformation && <HelperInformation />}
+                  </MonthContainer>
+                </CalendarContainer>
+              )
+            }
+          </CalendarContext.Consumer>
+        )}
+      </ThemeContext.Consumer>
     );
   }
 }
