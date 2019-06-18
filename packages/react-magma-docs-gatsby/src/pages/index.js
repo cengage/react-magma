ed691f2566287d50147be2d00e0284b6cf0eb20e
Ryan Gillett <ryan.gillett@cengage.com>
2019-06-18 12:51:40 -0400

    fix(husky): fix husky hooks

--- packages/react-magma-docs-gatsby/src/pages/index.js
+++ packages/react-magma-docs-gatsby/src/pages/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
-import Layout from '../components/layout';
-import { IndexPageContent } from '../components/intro/index-page-content';
-import { Global, css } from '@emotion/core';
-import { ThemeContext } from 'react-magma-dom';
+import React from 'react'
+import Layout from '../components/layout'
+import { IndexPageContent } from '../components/intro/index-page-content'
+import { Global, css } from '@emotion/core'
+import { ThemeContext } from 'react-magma-dom'
 
-import "animate.css/animate.min.css";
+import '../components/animate.css'
 
 const IndexPage = () => (
   <ThemeContext.Consumer>
-      {theme => (
-        <Layout>
-          <Global styles={css`
+    {theme => (
+      <Layout>
+        <Global
+          styles={css`
             @import url('https://fonts.googleapis.com/css?family=Abel');
-            .content { background: ${theme.colors.neutral01}; color: ${theme.colors.neutral08}; padding: 0; overflow: hidden; } 
-            .content-article { background: linear-gradient(to bottom, ${theme.colors.foundation01} 0%, ${theme.colors.pop01} 100%);  margin: 0; max-width: none; padding: 0; width: auto; } 
-          `} />
-          <IndexPageContent />
-        </Layout>
+            .content {
+              background: ${theme.colors.neutral01};
+              color: ${theme.colors.neutral08};
+              padding: 0;
+              overflow: hidden;
+            }
+            .content-article {
+              background: linear-gradient(
+                to bottom,
+                ${theme.colors.foundation01} 0%,
+                ${theme.colors.pop01} 100%
+              );
+              margin: 0;
+              max-width: none;
+              padding: 0;
+              width: auto;
+            }
+          `}
+        />
+        <IndexPageContent />
+      </Layout>
     )}
   </ThemeContext.Consumer>
 )
