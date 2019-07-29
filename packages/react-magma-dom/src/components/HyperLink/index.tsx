3f53c71e1b6e583c77a1775bcef6cd9496b1a215
Smith, Joanne <joanne.smith@cengage.com>
2019-07-29 16:40:07 -0500

    refactor(hyperlink): use omit function to remove props

--- packages/react-magma-dom/src/components/HyperLink/index.tsx
+++ packages/react-magma-dom/src/components/HyperLink/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { ButtonStyles } from '../Button';
 import { css, jsx, ClassNames } from '@emotion/core';
-import { Omit } from '../utils';
+import { omit, Omit } from '../utils';
 import { ThemeContext } from '../../theme/ThemeContext';
 import { buttonStyles } from '../StyledButton';
 import { buildPropsWithDefaultButtonStyles } from '../StyledButton/styles';
@@ -48,7 +48,9 @@ export const HyperLink: React.FunctionComponent<
   HyperLinkProps
 > = React.forwardRef((props: HyperLinkProps, ref: any) => {
   const composedProps = buildPropsWithDefaultButtonStyles(props);
-  const { children, to, testId, styledAs, textTransform, ...other } = props;
+  const { children, to, styledAs, ...other } = props;
+
+  const rest = omit(['textTransform'], other);
 
   return (
     <ThemeContext.Consumer>
@@ -69,7 +71,7 @@ export const HyperLink: React.FunctionComponent<
           );
         } else {
           return (
-            <a {...other} href={to} css={composedStyle}>
+            <a {...rest} href={to} css={composedStyle}>
               {children}
             </a>
           );
