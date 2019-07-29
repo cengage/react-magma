5576dcc0e629ec4543fbecd01f061cabfd379b40
Smith, Joanne <joanne.smith@cengage.com>
2019-07-29 16:59:50 -0500

    refactor(hyperlink): use omit

--- packages/react-magma-dom/src/components/HyperLink/index.tsx
+++ packages/react-magma-dom/src/components/HyperLink/index.tsx
@@ -2,7 +2,7 @@
 import * as React from 'react';
 import { ButtonStyles } from '../Button';
 import { css, jsx, ClassNames } from '@emotion/core';
-import { Omit } from '../utils';
+import { omit, Omit } from '../utils';
 import { ThemeContext } from '../../theme/ThemeContext';
 import { buttonStyles } from '../StyledButton';
 import { buildPropsWithDefaultButtonStyles } from '../StyledButton/styles';
@@ -50,17 +50,9 @@ export const HyperLink: React.FunctionComponent<
   HyperLinkProps
 > = React.forwardRef((props: HyperLinkProps, ref: any) => {
   const composedProps = buildPropsWithDefaultButtonStyles(props);
-  const {
-    children,
-    to,
-    testId,
-    styledAs,
-    textTransform,
-    inverse,
-    positionLeft,
-    positionTop,
-    ...other
-  } = props;
+  const { children, to, styledAs, inverse, ...other } = props;
+
+  const rest = omit(['textTransform'], other);
 
   return (
     <ThemeContext.Consumer>
@@ -81,7 +73,7 @@ export const HyperLink: React.FunctionComponent<
           );
         } else {
           return (
-            <a {...other} href={to} css={composedStyle}>
+            <a {...rest} href={to} css={composedStyle}>
               {children}
             </a>
           );
