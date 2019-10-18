14d484ad3382259405aabecf7a867ac28dc91386
Smith, Joanne <joanne.smith@cengage.com>
2019-10-18 11:12:43 -0500

    feat(search): helper message

--- packages/react-magma-dom/src/components/Search/index.tsx
+++ packages/react-magma-dom/src/components/Search/index.tsx
@@ -4,6 +4,7 @@ import { Search2Icon } from '../Icon/types/Search2Icon';
 
 export interface SearchProps extends React.HTMLAttributes<HTMLInputElement> {
   errorMessage?: string;
+  helperMessage?: string;
   iconAriaLabel?: string;
   id?: string;
   inputSize?: InputSize;
@@ -16,6 +17,7 @@ export interface SearchProps extends React.HTMLAttributes<HTMLInputElement> {
 
 export const Search: React.FunctionComponent<SearchProps> = ({
   errorMessage,
+  helperMessage,
   iconAriaLabel,
   id,
   inverse,
@@ -37,6 +39,7 @@ export const Search: React.FunctionComponent<SearchProps> = ({
   return (
     <Input
       errorMessage={errorMessage}
+      helperMessage={helperMessage}
       icon={<Search2Icon />}
       onIconClick={onSearch}
       iconAriaLabel={iconAriaLabel ? iconAriaLabel : SEARCH}
