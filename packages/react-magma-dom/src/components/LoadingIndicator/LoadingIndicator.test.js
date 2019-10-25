2258023deb95f530be4cf0ffb257a5dec4eb6a2e
Smith, Joanne <joanne.smith@cengage.com>
2019-10-25 16:07:12 -0500

    feat(loading indicator): cutom messages the change after time

--- packages/react-magma-dom/src/components/LoadingIndicator/LoadingIndicator.test.js
+++ packages/react-magma-dom/src/components/LoadingIndicator/LoadingIndicator.test.js
@@ -10,6 +10,51 @@ describe('Loading Indicator', () => {
     expect(container).toBeInTheDocument();
   });
 
+  it('should render the element with custom messages', () => {
+    const text1 = 'test 1';
+    const text2 = 'test 2';
+    const text3 = 'test 3';
+
+    const { getByText } = render(
+      <LoadingIndicator message1={text1} message2={text2} message3={text3} />
+    );
+
+    expect(getByText(text1)).toBeInTheDocument();
+    expect(getByText(text1)).toHaveStyleRule('opacity', '1');
+
+    expect(getByText(text2)).toBeInTheDocument();
+    expect(getByText(text2)).toHaveStyleRule('opacity', '0');
+
+    expect(getByText(text3)).toBeInTheDocument();
+    expect(getByText(text3)).toHaveStyleRule('opacity', '0');
+  });
+
+  it('should change the message after 5 and 15 seconds', () => {
+    const text1 = 'test 1';
+    const text2 = 'test 2';
+    const text3 = 'test 3';
+
+    const { getByText } = render(
+      <LoadingIndicator message1={text1} message2={text2} message3={text3} />
+    );
+
+    expect(getByText(text1)).toHaveStyleRule('opacity', '1');
+    expect(getByText(text2)).toHaveStyleRule('opacity', '0');
+    expect(getByText(text3)).toHaveStyleRule('opacity', '0');
+
+    setTimeout(() => {
+      expect(getByText(text1)).toHaveStyleRule('opacity', '0');
+      expect(getByText(text2)).toHaveStyleRule('opacity', '1');
+      expect(getByText(text3)).toHaveStyleRule('opacity', '0');
+    }, 6000);
+
+    setTimeout(() => {
+      expect(getByText(text1)).toHaveStyleRule('opacity', '0');
+      expect(getByText(text2)).toHaveStyleRule('opacity', '1');
+      expect(getByText(text3)).toHaveStyleRule('opacity', '0');
+    }, 16000);
+  });
+
   it('Does not violate accessibility standards', () => {
     const { container } = render(<LoadingIndicator />);
 
--- packages/react-magma-dom/src/components/LoadingIndicator/index.tsx
+++ packages/react-magma-dom/src/components/LoadingIndicator/index.tsx
@@ -4,7 +4,14 @@ import styled from '@emotion/styled';
 
 export interface LoadingIndicatorProps
   extends React.HTMLAttributes<HTMLDivElement> {
+  message1?: string;
+  message2?: string;
+  message3?: string;
   ref?: any;
+}
+
+export interface LoadingIndicatorState {
+  messageLevel: number;
 }
 
 const StyledLoadingIndicator = styled.div`
@@ -12,17 +19,77 @@ const StyledLoadingIndicator = styled.div`
   text-align: center;
 `;
 
-const Message = styled.div`
-  margin-top: 15px;
+const MessageContainer = styled.div`
+  margin-top: 20px;
+  max-width: 230px;
+  min-height: 5em;
+  position: relative;
+  text-align: center;
 `;
 
-export const LoadingIndicator: React.FunctionComponent<
+const Message = styled.div<{ hide?: boolean }>`
+  opacity: ${props => (props.hide ? '0' : '1')};
+  position: absolute;
+  transition: opacity 0.3s;
+  width: 100%;
+`;
+
+class LoadingIndicatorComponent extends React.Component<
+  LoadingIndicatorProps,
+  LoadingIndicatorState
+> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      messageLevel: 1
+    };
+
+    this.setTimer = this.setTimer.bind(this);
+  }
+
+  componentDidMount() {
+    this.setTimer();
+  }
+
+  setTimer() {
+    setTimeout(() => {
+      this.setState({ messageLevel: 2 });
+    }, 5000);
+
+    setTimeout(() => {
+      this.setState({ messageLevel: 3 });
+    }, 15000);
+  }
+
+  render() {
+    const { ref, message1, message2, message3 } = this.props;
+    const { messageLevel } = this.state;
+
+    return (
+      <StyledLoadingIndicator ref={ref}>
+        <Spinner size={50} />
+        <MessageContainer>
+          <Message hide={messageLevel !== 1}>
+            {message1 ? message1 : 'Loading...'}
+          </Message>
+          <Message hide={messageLevel !== 2}>
+            {message2
+              ? message2
+              : 'Thank you for your patience. Still loading...'}
+          </Message>
+          <Message hide={messageLevel !== 3}>
+            {message3
+              ? message3
+              : 'Sorry for the delay. This is taking longer than expected.'}
+          </Message>
+        </MessageContainer>
+      </StyledLoadingIndicator>
+    );
+  }
+}
+
+export const LoadingIndicator = React.forwardRef<
+  HTMLDivElement,
   LoadingIndicatorProps
-> = React.forwardRef(({  }: LoadingIndicatorProps, ref: any) => {
-  return (
-    <StyledLoadingIndicator ref={ref}>
-      <Spinner size={50} />
-      <Message>Loading...</Message>
-    </StyledLoadingIndicator>
-  );
-});
+>((props, ref) => <LoadingIndicatorComponent ref={ref} {...props} />);
