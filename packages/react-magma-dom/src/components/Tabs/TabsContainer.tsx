6f84da71e97af35b9b9f858c74669ad7c7fdeffd
Vadim Biliuk <vadim.biliuk@cengage.com>
2020-01-10 11:58:31 +0200

    fix(tabs): minor fix

--- packages/react-magma-dom/src/components/Tabs/TabsContainer.tsx
+++ packages/react-magma-dom/src/components/Tabs/TabsContainer.tsx
@@ -24,60 +24,6 @@ interface Action {
   };
 }
 
-// export const darkTheme = (tabsTheme: any) => {
-//   return {
-//     bgColor: tabsTheme.colors.neutral01,
-//     color: tabsTheme.colors.neutral08,
-//     opacity: '70%',
-//     hoverOpacity: '100%',
-//     activeColor: tabsTheme.colors.neutral08,
-//     hoverColor: tabsTheme.colors.neutral08,
-//     colorBorder: tabsTheme.colors.pop02,
-//     bgHoverColor: tabsTheme.colors.shade02,
-//     font: tabsTheme.bodyFont,
-//     boxShadow: '0px 5px 13px 1px rgba(0,0,0,0.10)',
-//     border: 'none',
-//     colorOfArrows: tabsTheme.colors.neutral08,
-//     focusColor: tabsTheme.colors.focus
-//   };
-// };
-
-// export const lightTheme = (tabsTheme: any) => {
-//   return {
-//     bgColor: tabsTheme.colors.neutral08,
-//     color: tabsTheme.colors.neutral01,
-//     opacity: '70%',
-//     hoverOpacity: '70%',
-//     hoverColor: tabsTheme.colors.neutral01,
-//     activeColor: tabsTheme.colors.primary,
-//     colorBorder: tabsTheme.colors.primary,
-//     bgHoverColor: tabsTheme.colors.shade01,
-//     font: tabsTheme.bodyFont,
-//     boxShadow: 'none',
-//     border: `1px solid ${tabsTheme.colors.neutral06}`,
-//     colorOfArrows: tabsTheme.colors.neutral02,
-//     focusColor: tabsTheme.colors.focus
-//   };
-// };
-
-// export const blueTheme = (tabsTheme: any) => {
-//   return {
-//     bgColor: tabsTheme.colors.foundation02,
-//     color: tabsTheme.colors.neutral08,
-//     opacity: '70%',
-//     hoverOpacity: '100%',
-//     activeColor: tabsTheme.colors.neutral08,
-//     hoverColor: tabsTheme.colors.neutral08,
-//     colorBorder: tabsTheme.colors.pop02,
-//     bgHoverColor: 'rgba(0,0,0,0.25)',
-//     font: tabsTheme.bodyFont,
-//     boxShadow: '0px 5px 13px 1px rgba(0,0,0,0.10)',
-//     border: 'none',
-//     colorOfArrows: tabsTheme.colors.neutral08,
-//     focusColor: tabsTheme.colors.focus
-//   };
-// };
-
 export const TabsContext = React.createContext({} as {
   state: State;
   dispatch: React.Dispatch<Action>;
