9c45f52a3e5debbfc885599baf40fd8622eb5346
Ryan Gillett <ryan.gillett@cengage.com>
2020-04-10 11:17:54 -0400

    test(tabs): add tests for utils

---
+++ packages/react-magma-dom/src/utils/utils.test.js
@@ -0,0 +1,62 @@
+import { debounce, getNormalizedScrollLeft } from '.';
+
+describe('Utils', () => {
+  describe('Debounce', () => {
+    beforeEach(() => {
+      jest.useFakeTimers();
+    });
+
+    afterEach(() => {
+      jest.useRealTimers();
+    });
+
+    it('should call function passed in to debounce after a delay', () => {
+      const func = jest.fn();
+
+      const debouncedFunc = debounce(func, 100);
+
+      debouncedFunc();
+
+      jest.runAllTimers();
+
+      expect(func).toHaveBeenCalled();
+    });
+
+    it('should not implement the debounced function twice within the same delay window', () => {
+      const func = jest.fn();
+
+      const debouncedFunc = debounce(func, 100);
+
+      debouncedFunc();
+      debouncedFunc();
+
+      jest.runAllTimers();
+
+      expect(func).toHaveBeenCalledTimes(1);
+    });
+
+    it('should clear the timeout when the clear function is called', () => {
+      window.clearTimeout = jest.fn();
+      const func = jest.fn();
+
+      const debouncedFunc = debounce(func, 100);
+
+      debouncedFunc.clear();
+
+      expect(window.clearTimeout).toHaveBeenCalled();
+      window.clearTimeout.mockRestore();
+    });
+  });
+
+  describe('getNormalizedScrollLeft', () => {
+    it('get the scroll left for ltr direction', () => {
+      const scrollLeft = 1;
+      const normalizedScrollLeft = getNormalizedScrollLeft(
+        { scrollLeft },
+        'ltr'
+      );
+
+      expect(normalizedScrollLeft).toEqual(scrollLeft);
+    });
+  });
+});
