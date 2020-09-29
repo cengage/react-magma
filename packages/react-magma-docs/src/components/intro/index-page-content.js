903a63a83c6c20dcf92374c22ea6c018efa765b5
Smith, Joanne <joanne.smith@cengage.com>
2020-09-29 08:27:57 -0500

    docs(nav): add aria-labels to docs navigation

--- packages/react-magma-docs/src/components/intro/index-page-content.js
+++ packages/react-magma-docs/src/components/intro/index-page-content.js
@@ -126,7 +126,7 @@ export class IndexPageContent extends React.Component {
       <ThemeContext.Consumer>
         {theme => (
           <>
-            <nav>
+            <nav aria-label="Intro to React Magma navigation">
               <List theme={theme}>
                 <NavItem
                   activeSection={this.state.activeSection}
