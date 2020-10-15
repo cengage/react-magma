cd687970be84d82244669fdb8591d60bf33fb65b
Aaron Maturen <aaron.maturen@cengage.com>
2020-10-15 15:06:55 -0400

    fix(docs): move skiplink back up to first

--- packages/react-magma-docs/gatsby-ssr.js
+++ packages/react-magma-docs/gatsby-ssr.js
@@ -8,15 +8,38 @@ import React from "react";
 import Layout from "./src/components/layout";
 import { SlidingDrawer } from './src/components/sliding-drawer';
 import Masthead from './src/components/masthead';
+import { SkipLink, GlobalStyles } from 'react-magma-dom';
+import styled from '@emotion/styled';
+
+const StyledSkipLink = styled(SkipLink)`
+  display: none;
+
+  &:not(:disabled):focus {
+    background: transparent;
+  }
+
+  @media (min-width: 1024px) {
+    display: inline-flex;
+  }
+`;
 
 export const wrapRootElement = ({ element, props }) => {
     // props provide same data to Layout as Page element will get
     // including location, data, etc - you don't need to pass it
-    return <div className="main-container">
-      <Masthead />
-      <SlidingDrawer />
-      <Layout {...props}>
-        {element}
-      </Layout>
-    </div>;
+    return <>
+      <GlobalStyles />
+      <StyledSkipLink
+        isInverse
+        positionLeft={275}
+        positionTop={16}
+        variant="outline"
+      />
+      <div className="main-container">
+        <Masthead />
+        <SlidingDrawer />
+        <Layout {...props}>
+          {element}
+        </Layout>
+      </div>
+    </>;
 };
--- packages/react-magma-docs/src/components/layout-component/index.js
+++ packages/react-magma-docs/src/components/layout-component/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import { SkipLink, GlobalStyles } from 'react-magma-dom';
-import styled from '@emotion/styled';
-
-const StyledSkipLink = styled(SkipLink)`
-  display: none;
-
-  &:not(:disabled):focus {
-    background: transparent;
-  }
-
-  @media (min-width: 1024px) {
-    display: inline-flex;
-  }
-`;
 
 export default props => (
   <>
-    <GlobalStyles />
     <Helmet
       title={props.title ? `${props.title} - React Magma` : 'React Magma'}
       meta={[
@@ -27,14 +12,8 @@ export default props => (
     >
       <html lang="en" />
     </Helmet>
-    <StyledSkipLink
-      isInverse
-      positionLeft={275}
-      positionTop={16}
-      variant="outline"
-    />
-      <main>
-        <section className="content">{props.children}</section>
-      </main>
+    <main>
+      <section className="content">{props.children}</section>
+    </main>
   </>
 );
