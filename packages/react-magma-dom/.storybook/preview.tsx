e0d2c99442f9a3a2ead46f7806fe73cabd6b7d64
Maturen, Aaron T <aaron.maturen@cengage.com>
2021-01-20 11:29:49 -0500

    docs(docs): add settings for codesandbox and storybook

---
+++ packages/react-magma-dom/.storybook/preview.tsx
@@ -0,0 +1,15 @@
+import { StoryContext } from '@storybook/react';
+import * as React from 'react';
+import { GlobalStyles } from '../src/theme/GlobalStyles';
+import { withPerformance } from 'storybook-addon-performance';
+
+const withMagma = (StoryFn: Function, context: StoryContext) => {
+  return (
+    <div>
+      <GlobalStyles />
+      <StoryFn />
+    </div>
+  );
+};
+
+export const decorators = [withMagma, withPerformance];
