473e608aed0f64fd60e9b29cca2462e449371ef1
Maturen, Aaron T <aaron.maturen@cengage.com>
2021-01-25 12:31:49 -0500

    fix(storybook): fix previews for storybook
    re #114

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ const toPath = _path => path.join(process.cwd(), _path);
 module.exports = {
   stories: ['../packages/**/src/components/**/*.stories.@(ts|tsx)'],
   addons: [
+    '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
     'storybook-addon-performance/register',
