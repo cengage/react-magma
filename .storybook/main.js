1672f0074d57c0b15de1ac05eadd7c9760bc360b
Aaron Maturen <aaron.maturen@cengage.com>
2021-01-26 13:46:57 -0500

    fix(storybook): fix config for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,7 +7,6 @@ module.exports = {
     '@storybook/addon-docs',
     '@storybook/addon-actions',
     '@storybook/addon-links',
-    '@storybook/addon-docs',
     'storybook-addon-performance/register',
     '@storybook/addon-a11y',
     '@storybook/addon-toolbars',
