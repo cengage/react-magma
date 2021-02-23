24cf9bd66d8600fa920c09f42dcc48aab1f7f585
Joanne Smith <joanne.smith@cengage.com>
2021-02-23 12:32:35 -0600

    docs(indeterminate checkbox): fix type errors in indeterminate checkbox examples

--- packages/react-magma-dom/src/components/Checkbox/Checkbox.stories.tsx
+++ packages/react-magma-dom/src/components/Checkbox/Checkbox.stories.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { Checkbox } from '.';
+import {
+  IndeterminateCheckbox,
+  IndeterminateCheckboxStatus,
+} from '../IndeterminateCheckbox';
 import { FormGroup } from '../FormGroup';
+import { magma } from '../../theme/magma';
 
 export default {
   component: Checkbox,
@@ -25,5 +30,54 @@ export const Default = () => {
 
       <Checkbox labelText="Checkbox label is really long and can wrap to multiple lines lorem ipsum dolar sit amet is really long and can wrap to multiple lines" />
     </FormGroup>
+  );
+};
+
+export const Indeterminate = () => {
+  const [checkedItems, setCheckedItems] = React.useState<Array<boolean>>([
+    true,
+    false,
+  ]);
+
+  const status: IndeterminateCheckboxStatus = checkedItems.every(Boolean)
+    ? IndeterminateCheckboxStatus.checked
+    : checkedItems.some(Boolean)
+    ? IndeterminateCheckboxStatus.indeterminate
+    : IndeterminateCheckboxStatus.unchecked;
+
+  function handleUpdateIndeterminateChecked(
+    event: React.ChangeEvent<HTMLInputElement>
+  ) {
+    setCheckedItems([event.target.checked, event.target.checked]);
+  }
+
+  function handleUpdateRedChecked(event: React.ChangeEvent<HTMLInputElement>) {
+    setCheckedItems([event.target.checked, checkedItems[1]]);
+  }
+
+  function handleUpdateBlueChecked(event: React.ChangeEvent<HTMLInputElement>) {
+    setCheckedItems([checkedItems[0], event.target.checked]);
+  }
+
+  return (
+    <>
+      <IndeterminateCheckbox
+        onChange={handleUpdateIndeterminateChecked}
+        status={status}
+        labelText="Colors"
+      />
+      <div style={{ marginLeft: magma.spaceScale.spacing08 }}>
+        <Checkbox
+          checked={checkedItems[0]}
+          onChange={handleUpdateRedChecked}
+          labelText="Red"
+        />
+        <Checkbox
+          checked={checkedItems[1]}
+          onChange={handleUpdateBlueChecked}
+          labelText="Blue"
+        />
+      </div>
+    </>
   );
 };
