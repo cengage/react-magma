02634c933dcf10fd1000d6d3da60850409957579
Aaron Maturen <aaron.maturen@cengage.com>
2021-03-18 11:43:50 -0400

    docs(header): fix previews/codesandbox

--- website/react-magma-docs/src/components/CodeBlock/preview.tsx
+++ website/react-magma-docs/src/components/CodeBlock/preview.tsx
@@ -7,7 +7,7 @@ import * as MAGMA from 'react-magma-dom';
 import * as ICONS from 'react-magma-icons';
 import styled from '@emotion/styled';
 import { es, zhCN } from 'date-fns/locale';
-import { Header } from '@cengage-patterns/header';
+import * as HEADER from '@cengage-patterns/header';
 
 import { CodeBlockContext } from './context';
 
@@ -47,7 +47,7 @@ export const Preview = ({ ...props }: PreviewProps) => {
     <PreviewContainer noBorder={props.noBorder}>
       <LiveProvider
         code={preview.code}
-        scope={{ es, zhCN, styled, ...MAGMA, ...ICONS, ICONS, Header }}
+        scope={{ es, zhCN, styled, ...MAGMA, ...ICONS, ICONS, ...HEADER }}
         noInline
         transformCode={transformCode}
       >
