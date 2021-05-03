06b8993c58df9588db033493fbeb0141032e9b4c
Maturen, Aaron T <aaron.maturen@cengage.com>
2021-05-03 15:41:27 -0400

    chore(icons): move out of the monorepo

--- packages/react-magma-icons/src/IconProps.ts
+++
@@ -1,9 +0,0 @@
-import { CSSProperties } from 'react';
-export interface IconProps {
-  id?: string;
-  style?: CSSProperties;
-  testId?: string;
-  title?: string;
-  color?: string;
-  size?: number;
-}
