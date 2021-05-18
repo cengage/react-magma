57dc3c142b322da37515ef4427a28a7920f3b2aa
Aaron Maturen <aaron.maturen@cengage.com>
2021-05-18 12:10:50 -0400

    fix: remove unused

--- packages/react-magma-schema-renderer/src/components/BasicTemplate/index.tsx
+++ packages/react-magma-schema-renderer/src/components/BasicTemplate/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import { Heading } from 'react-magma-dom';
 import { FormTemplateRenderProps } from '@data-driven-forms/react-form-renderer';
 
 export const BasicTemplate = ({
   formFields,
-  schema,
 }: FormTemplateRenderProps) => {
   return (
     <div>
