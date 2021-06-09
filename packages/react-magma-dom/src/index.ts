863528f01d056582cb8f1e0184d133aebca50062
Ryan Gillett <ryan.gillett@cengage.com>
2021-06-09 12:18:28 -0400

    fix(datagrid): exporting datagrid and useDataPagination

--- packages/react-magma-dom/src/index.ts
+++ packages/react-magma-dom/src/index.ts
@@ -44,6 +44,7 @@ export {
   CheckboxProps,
 } from './components/Checkbox';
 export { Container, ContainerProps } from './components/Container';
+export { Datagrid, DatagridProps } from './components/Datagrid';
 export { DatePicker, DatePickerProps } from './components/DatePicker';
 export {
   Dropdown,
@@ -215,6 +216,7 @@ export { I18nContext } from './i18n';
 export { defaultI18n } from './i18n/default';
 export { I18nInterface } from './i18n/interface';
 export { useMediaQuery } from './hooks/useMediaQuery';
+export { useDataPagination } from './hooks/useDataPagination';
 export { usePagination } from './hooks/usePagination';
 export { InverseContext, useIsInverse } from './inverse';
 
