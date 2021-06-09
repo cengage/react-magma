a83ad0290cdf840eeb7a0bbfec91e64e230be478
Chris Cedrone <chris.cedrone@cengage.com>
2021-06-09 12:25:38 -0400

    feat(tag): adding initial branch for design review

---
+++ packages/react-magma-dom/src/components/Tag/Tag.tsx
@@ -0,0 +1,220 @@
+import * as React from 'react';
+import styled from '../../theme/styled';
+import { css } from '@emotion/core';
+
+import { CancelIcon, IconProps } from 'react-magma-icons';
+import { ThemeContext } from '../../theme/ThemeContext';
+import { ThemeInterface } from '../../theme/magma';
+import { useIsInverse } from '../../inverse';
+import { Omit, XOR } from '../../utils';
+// import { I18nContext } from '../../i18n';
+
+/**
+ * @children required
+ */
+
+/*
+
+  TO DO
+
+  * Internationalization
+
+  * If it's clickable, wrap in <button>, if not wrap in <span>
+
+  * Move CSS to Magma.ts
+
+Clickable determination:
+
+line 83 
+
+line 98
+
+Import XOR (line 4) (edited) 
+
+Set up BaseTagProps (line 53) (edited) 
+
+Set up TagClickEventProps (which is an xor line 83)
+
+Return as your type TagProps = BaseTagProps & TagClickEventProps (line 98)
+
+*/
+
+export enum TagColor {
+  default = 'default',
+  primary = 'primary',
+  lowContrast = 'lowContrast',
+  highContrast = 'highContrast',
+}
+
+export enum TagSize {
+  default = 'default',
+  small = 'small',
+}
+
+export interface BaseTagProps
+  extends Omit<React.HTMLAttributes<HTMLButtonElement>, 'onClick'> {
+  color?: TagColor;
+  disabled?: boolean;
+  testId?: string;
+  icon?: React.ReactElement<IconProps>;
+  isClickable?: boolean;
+  isInverse?: boolean;
+  size?: TagSize;
+  /**
+   * @internal
+   */
+  theme?: ThemeInterface;
+}
+
+export interface DeletableTagProps extends BaseTagProps {
+  onDelete: () => void;
+}
+
+export interface ClickableTagProps extends BaseTagProps {
+  onClick?: () => void;
+}
+
+export type TagProps = XOR<DeletableTagProps, ClickableTagProps>;
+//////
+
+function buildButtonBackground(props) {
+  if (props.isInverse) {
+    switch (props.color) {
+      case 'primary':
+        return `background: ${props.theme.colors.primaryInverse}; 
+                color:${props.theme.colors.neutral};`;
+      case 'lowContrast':
+        return `background: none; 
+                box-shadow:inset 0 0 0 1px ${props.theme.colors.tint04}; 
+                color:${props.theme.colors.neutral08};`;
+      case 'highContrast':
+        return `background: ${props.theme.colors.neutral08}; 
+                color:${props.theme.colors.neutral};`;
+      default:
+        return `background: ${props.theme.colors.neutral03}; 
+                color:${props.theme.colors.neutral08};`;
+    }
+  }
+  switch (props.color) {
+    case 'primary':
+      return `background: ${props.theme.colors.primary}; 
+              color:${props.theme.colors.neutral08};`;
+    case 'lowContrast':
+      return `background: ${props.theme.colors.neutral08}; 
+              box-shadow:inset 0 0 0  1px ${props.theme.colors.neutral05}`;
+    case 'highContrast':
+      return `background: ${props.theme.colors.neutral}; 
+              color:${props.theme.colors.neutral08};`;
+    default:
+      return `background: ${props.theme.colors.neutral06};`;
+  }
+}
+
+function buildTagPadding(props) {
+  if (props.icon) {
+    switch (props.size) {
+      case 'small':
+        return `2px`;
+      default:
+        return `${props.theme.spaceScale.spacing02} 6px ${props.theme.spaceScale.spacing02} ${props.theme.spaceScale.spacing02}`;
+    }
+  }
+  switch (props.size) {
+    case 'small':
+      return `0 ${props.theme.spaceScale.spacing03}`;
+    default:
+      return `${props.theme.spaceScale.spacing02} ${props.theme.spaceScale.spacing02}`;
+  }
+}
+
+const TagStyling = props => css`
+  border: 0;
+  border-radius: ${props.theme.spaceScale.spacing05};
+  ${buildButtonBackground(props)};
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-around;
+  font-size: ${props.size === 'small'
+    ? `${props.theme.typeScale.size01.fontSize}`
+    : `${props.theme.typeScale.size02.fontSize}`};
+  font-weight: ${props.size === 'small' ? `600` : `inherit`};
+  opacity: ${props.disabled ? '60%' : 'inherit'};
+  padding: ${buildTagPadding(props)};
+  svg:last-child {
+    opacity: ${props.onDelete ? '0.75' : 'inherit'};
+  }
+`;
+
+const StyledButton = styled.button<{
+  disabled?: boolean;
+  onDelete?: boolean;
+  isInverse?: boolean;
+  size: string;
+}>`
+  ${TagStyling};
+  cursor: pointer;
+`;
+
+const StyledSpan = styled.span<{
+  disabled?: boolean;
+  isInverse?: boolean;
+  size: string;
+}>`
+  ${TagStyling};
+`;
+
+function getStyledTag(isClickable: boolean) {
+  return isClickable ? StyledButton : StyledSpan;
+}
+
+export const Tag = React.forwardRef<HTMLButtonElement, TagProps>(
+  (props, ref) => {
+    const {
+      children,
+      color,
+      onClick,
+      onDelete,
+      isInverse: isInverseProp,
+      size = TagSize.default,
+      testId,
+      ...rest
+    } = props;
+    const theme = React.useContext(ThemeContext);
+    const isInverse = useIsInverse(isInverseProp);
+    // const i18n = React.useContext(I18nContext);
+
+    const leftIcon = props.icon;
+
+    const LabelWrap = styled.span`
+      padding: 0 10px;
+    `;
+
+    const StyledTag = getStyledTag(Boolean(onClick));
+
+    function handleClick() {
+      if (onClick && typeof onClick === 'function') {
+        onClick();
+      } else if (onDelete && typeof onDelete === 'function') {
+        onDelete();
+      }
+    }
+
+    return (
+      <StyledTag
+        color={color}
+        icon={leftIcon}
+        theme={theme}
+        onClick={handleClick}
+        isInverse={isInverse}
+        ref={ref}
+        data-testid={props.testId}
+        size={size}
+        {...rest}
+      >
+        {leftIcon}
+        <LabelWrap>{children}</LabelWrap>
+        {onDelete && <CancelIcon size={theme.iconSizes.small} />}
+      </StyledTag>
+    );
+  }
+);
--- packages/react-magma-dom/src/index.ts
+++ packages/react-magma-dom/src/index.ts
@@ -183,6 +183,8 @@ export { Tab } from './components/Tabs/Tab';
 export { TabPanelsContainer } from './components/Tabs/TabPanelsContainer';
 export { TabPanel } from './components/Tabs/TabPanel';
 
+export { Tag, TagProps } from './components/Tag';
+
 export {
   ToastsContainer,
   ToastsContainerProps,
@@ -222,3 +224,4 @@ export * from './components/Transition';
 export * from './components/Drawer';
 
 export * from './components/FormFieldContainer';
+export * from './components/Tag'
