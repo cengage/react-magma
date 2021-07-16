7b381e7dba867586df50734952e5c321d023d394
Jamie Smylnycky <7326161+jamiesmyln@users.noreply.github.com>
2021-07-16 11:25:58 -0400

    refactor(grid): grid

--- packages/react-magma-dom/src/components/Grid/Grid.test.js
+++ packages/react-magma-dom/src/components/Grid/Grid.test.js
@@ -2,12 +2,11 @@ import React from 'react';
 import { axe } from 'jest-axe';
 import { Grid, GridItem } from '.';
 import { render } from '@testing-library/react';
-import { I18nContext, defaultI18n } from '../../i18n';
 
 const TEXT = 'Test Text';
 
 describe('Grid', () => {
-  it('should render the visually hidden component', () => {
+  it('should render the grid component', () => {
     const { getByText } = render(<Grid>{TEXT}</Grid>);
 
     expect(getByText(TEXT)).toBeInTheDocument();
@@ -21,20 +20,13 @@ describe('Grid', () => {
 
   it('should render a grid item with the correct styles', () => {
     const { getByText } = render(
-      <Grid gridColumns="repeat(12, 1fr)">
-        <GridItem gridColSpan="1 / 6">{TEXT}</GridItem>
+      <Grid gridtemplateColumns="repeat(12, 1fr)">
+        <GridItem gridColumn="1 / 6">{TEXT}</GridItem>
       </Grid>
     );
 
     expect(getByText(TEXT).parentElement).toHaveStyleRule('display', 'grid');
     expect(getByText(TEXT)).toHaveStyleRule('grid-column', '1 / 6');
-  });
-
-  it('should find element by testId', () => {
-    const testId = 'test-id';
-    const { getByTestId } = render(<Grid testId={testId}>{TEXT}</Grid>);
-
-    expect(getByTestId(testId)).toBeInTheDocument();
   });
 
   it('Does not violate accessibility standards', () => {
