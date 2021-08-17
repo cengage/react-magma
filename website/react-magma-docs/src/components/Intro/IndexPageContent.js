45faff00e24fc96f742746044dbe0f62f71b104b
Jamie Smylnycky <7326161+jamiesmyln@users.noreply.github.com>
2021-08-17 16:41:37 -0400

    Docs home page

--- website/react-magma-docs/src/components/Intro/IndexPageContent.js
+++ website/react-magma-docs/src/components/Intro/IndexPageContent.js
@@ -1,16 +1,28 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
-import { Grid, GridItem } from 'react-magma-dom';
-import { Heading } from 'react-magma-dom';
 import { Button } from 'react-magma-dom';
 import { Card, CardBody, CardHeading } from 'react-magma-dom';
+import { Flex } from 'react-magma-dom';
+import { Grid, GridItem } from 'react-magma-dom';
+import { Heading, TypographyVisualStyle } from 'react-magma-dom';
+import {
+  ArchitectureIcon,
+  CodeIcon,
+  AccessibilityIcon,
+  PaletteIcon,
+  DevicesIcon,
+  ExtensionIcon,
+  TimelineIcon,
+  GroupsIcon,
+} from 'react-magma-icons';
+import { Logo } from '../Logo';
 import { magma } from 'react-magma-dom';
-import styled from '@emotion/styled-base';
-import { FullLogo } from '../FullLogo';
+import styled from '@emotion/styled';
 
 const StyledGrid = styled(Grid)`
   grid-template-columns: auto auto;
-  padding: 2em 1em;
+  grid-template-rows: auto;
+  padding: 2em;
 
   @media (max-width: ${magma.breakpoints.small}px) {
     display: block;
@@ -27,113 +39,205 @@ const StyledGridItem = styled(GridItem)`
   }
 `;
 
+const HeaderBlock = styled.div`
+  display: flex;
+  align-items: center;
+  padding: ${magma.spaceScale.spacing06};
+
+  svg {
+    justify-self: flex-end;
+    padding: 0 1em 0 2em;
+    height: 200px;
+    width: 200px;
+    margin: auto;
+  }
+
+  @media (max-width: ${magma.breakpoints.small}px) {
+    svg {
+      display: none;
+    }
+  }
+`;
+
+const CenterBlock = styled.div`
+  grid-column: 1 / 3;
+  text-align: center;
+  margin: auto;
+  max-width: 400px;
+
+  button {
+    margin: auto;
+  }
+`;
+
+const CardGrid = styled(Card)`
+  display: grid;
+  grid-template-columns: min-content auto;
+`;
+
+const CardButton = styled(Button)`
+  display: flex;
+  margin-top: 2em;
+`;
+
+const backgroundColors = {
+  yellow: magma.colors.pop04,
+  pink: magma.colors.pop,
+  teal: magma.colors.pop06,
+  blue: magma.colors.foundation02,
+  green: magma.colors.success02,
+  orange: magma.colors.pop02,
+  purple: magma.colors.pop05,
+};
+
+const setCardIconColor = props => {
+  return backgroundColors[props.color] || backgroundColors.yellow;
+};
+
+const CardIcon = styled(Flex)`
+  height: 72px;
+  width: 72px;
+  border-radius: 50%;
+  margin: ${magma.spaceScale.spacing06};
+  background-color: ${props => setCardIconColor(props)};
+
+  > * {
+    align-self: center;
+    justify-self: center;
+    margin: auto;
+    height: ${magma.iconSizes.xLarge}px;
+    width: ${magma.iconSizes.xLarge}px;
+  }
+`;
+
 export function IndexPageContent() {
   return (
-    <StyledGrid gridTemplateRows="auto" gridGap={magma.spaceScale.spacing06}>
-      <StyledGridItem gridColumn="1 / 3">
-        <Heading level={1}>React Magma Design System</Heading>
-        <p>
-          React Magma is Cengage's open source design system for creating
-          digital experiences with React.
-        </p>
-        <FullLogo />
-      </StyledGridItem>
+    <>
+      <HeaderBlock>
+        <div>
+          <Heading level={1}>React Magma Design System</Heading>
+          <Heading level={2} visualStyle={TypographyVisualStyle.bodyLarge}>
+            React Magma is Cengage's open source design system for creating
+            digital experiences with React.
+          </Heading>
+        </div>
+        <Logo />
+      </HeaderBlock>
+      <StyledGrid gridGap={magma.spaceScale.spacing06}>
+        <StyledGridItem>
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="yellow">
+              <ArchitectureIcon />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Designing</CardHeading>
+              Start working in Sketch with ready-made UI components such as
+              inputs, buttons, and more! To see how a component works and
+              behaves, please refer to the usage guidelines on this site.
+              <CardButton>Start Designing</CardButton>
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
 
-      <StyledGridItem>
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Designing</CardHeading>
-            Start working in Sketch with ready-made UI components such as
-            inputs, buttons, and more! To see how a component works and behaves,
-            please refer to the usage guidelines on this site.
-            <Button style={{ display: 'flex', marginTop: '2em' }}>
-              Start Designing
-            </Button>
-          </CardBody>
-        </Card>
-      </StyledGridItem>
+        <StyledGridItem>
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="yellow">
+              <CodeIcon />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Developing</CardHeading>
+              Use the documentation on this site to guide your development, and
+              grab the code on github when you're ready to get started.
+              <CardButton>Start Developing</CardButton>
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
 
-      <StyledGridItem>
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Developing</CardHeading>
-            Use the documentation on this site to guide your development, and
-            grab the code on github when you're ready to get started.
-            <Button style={{ display: 'flex', marginTop: '2em' }}>
-              Start Developing
-            </Button>
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-
-      <StyledGridItem gridColumn="1 / 3" style={{ textAlign: 'center' }}>
-        <Heading level={2}>Working smarter, not harder</Heading>
-        <p>
-          Standardized cmponents support collaboration, reinforce branding, and
-          provide a consistent look and user experience.
-        </p>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="1">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Accessible</CardHeading>
-            Designed and developed for WCAG 2.1 compliance.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="2">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Themeable</CardHeading>
-            Easily theme all components with your brand's specific styles.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="1">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Platform Agnostic</CardHeading>
-            Design and develop experiences for any platform.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="2">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Scalable</CardHeading>
-            Manage design at scale with a design system that evolves as needs
-            change.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="1">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Efficient</CardHeading>
-            Save time so you can focus on larger issues of usability and
-            meaning.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="2">
-        <Card background={magma.colors.neutral07}>
-          <CardBody>
-            <CardHeading>Living</CardHeading>
-            Open source design system that is constantly expanding.
-          </CardBody>
-        </Card>
-      </StyledGridItem>
-      <StyledGridItem gridColumn="1 / 3" style={{ textAlign: 'center' }}>
-        <Heading level={2}>Want to contribute?</Heading>
-        <p>
-          We welcome all ideas and feedback to help us produce the best possible
-          experience for our users. If you're interested in contributing, review
-          our contribution guidelines to get started.
-        </p>
-        <Button style={{ margin: 'auto', maxWidth: 'fit-content' }}>
-          Start Contributing
-        </Button>
-      </StyledGridItem>
-    </StyledGrid>
+        <CenterBlock>
+          <Heading level={2}>Working smarter, not harder</Heading>
+          <p>
+            Standardized cmponents support collaboration, reinforce branding,
+            and provide a consistent look and user experience.
+          </p>
+        </CenterBlock>
+        <StyledGridItem gridColumn="1">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="pink">
+              <AccessibilityIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Accessible</CardHeading>
+              Designed and developed for WCAG 2.1 compliance.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <StyledGridItem gridColumn="2">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="teal">
+              <PaletteIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Themeable</CardHeading>
+              Easily theme all components with your brand's specific styles.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <StyledGridItem gridColumn="1">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="blue">
+              <DevicesIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Platform Agnostic</CardHeading>
+              Design and develop experiences for any platform.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <StyledGridItem gridColumn="2">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="green">
+              <ExtensionIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Scalable</CardHeading>
+              Manage design at scale with a design system that evolves as needs
+              change.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <StyledGridItem gridColumn="1">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="orange">
+              <TimelineIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Efficient</CardHeading>
+              Save time so you can focus on larger issues of usability and
+              meaning.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <StyledGridItem gridColumn="2">
+          <CardGrid background={magma.colors.neutral07}>
+            <CardIcon color="purple">
+              <GroupsIcon color="white" />
+            </CardIcon>
+            <CardBody>
+              <CardHeading>Living</CardHeading>
+              Open source design system that is constantly expanding.
+            </CardBody>
+          </CardGrid>
+        </StyledGridItem>
+        <CenterBlock>
+          <Heading level={2}>Want to contribute?</Heading>
+          <p>
+            We welcome all ideas and feedback to help us produce the best
+            possible experience for our users. If you're interested in
+            contributing, review our contribution guidelines to get started.
+          </p>
+          <Button>Start Contributing</Button>
+        </CenterBlock>
+      </StyledGrid>
+    </>
   );
 }
