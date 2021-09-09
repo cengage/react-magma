7328a1743b8743c16d83041c64f3731d594e0930
Jamie Smylnycky <7326161+jamiesmyln@users.noreply.github.com>
2021-09-09 15:01:06 -0400

    feat(file uploader): icon alignment fixes for the file uploader

--- packages/file-uploader/src/components/fileuploader/Preview.tsx
+++ packages/file-uploader/src/components/fileuploader/Preview.tsx
@@ -1,9 +1,4 @@
-import {
-  forwardRef,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
+import { forwardRef, useContext, useEffect, useState } from 'react';
 
 import {
   CheckCircleIcon,
@@ -32,7 +27,7 @@ import {
 import { FileIcon } from './FileIcon';
 import { FilePreview } from './FilePreview';
 
-export interface PreviewProps extends Omit<FlexProps, 'behavior'>{
+export interface PreviewProps extends Omit<FlexProps, 'behavior'> {
   file: FilePreview;
   isInverse?: boolean;
   onDeleteFile?: (file: FilePreview) => void;
@@ -41,8 +36,9 @@ export interface PreviewProps extends Omit<FlexProps, 'behavior'>{
   thumbnails: boolean;
 }
 
-const Thumb = styled.div<{file: FilePreview}>`
-  background-image: ${({file}) => `url('${'preview' in file && file.preview}')`};
+const Thumb = styled.div<{ file: FilePreview }>`
+  background-image: ${({ file }) =>
+    `url('${'preview' in file && file.preview}')`};
   background-repeat: no-repeat;
   background-size: contain;
   display: inline-block;
@@ -53,7 +49,7 @@ const Thumb = styled.div<{file: FilePreview}>`
 const StatusIcons = styled.div`
   display: grid;
   grid-template-areas: 'inner-div';
-  height: 42px;
+  height: auto;
   place-items: center;
   width: 42px;
   & > div {
@@ -64,12 +60,17 @@ const StatusIcons = styled.div`
 `;
 
 const Errors = styled.div`
-  border-top: 1px solid ${({theme}) => theme.colors.neutral04};
+  border-top: 1px solid ${({ theme }) => theme.colors.neutral04};
   padding: 10px;
 `;
 
 const StyledFlex = styled(Flex)`
-  padding: 10px;
+  height: 48px;
+  padding: 0 10px;
+
+  > div {
+    display: flex;
+  }
 `;
 
 const FileName = styled(Flex)`
@@ -77,10 +78,16 @@ const FileName = styled(Flex)`
   white-space: nowrap;
 `;
 
-const StyledCard = styled(Card)<{file: FilePreview, isInverse: boolean}>`
-  background-color: ${({isInverse, theme}) => isInverse ? theme.colors.foundation02 : theme.colors.neutral08};
-  border-color: ${({file, theme, isInverse}) => file.errors ? isInverse ? theme.colors.dangerInverse : theme.colors.danger : theme.colors.neutral06};
-  border-width: ${({file}) => file.errors ? '2px' : '1px'};
+const StyledCard = styled(Card)<{ file: FilePreview; isInverse: boolean }>`
+  background-color: ${({ isInverse, theme }) =>
+    isInverse ? theme.colors.foundation02 : theme.colors.neutral08};
+  border-color: ${({ file, theme, isInverse }) =>
+    file.errors
+      ? isInverse
+        ? theme.colors.dangerInverse
+        : theme.colors.danger
+      : theme.colors.neutral06};
+  border-width: ${({ file }) => (file.errors ? '2px' : '1px')};
   margin: 10px 0;
 `;
 
@@ -104,104 +111,134 @@ export const Preview = forwardRef<HTMLDivElement, PreviewProps>(
       ...rest
     } = props;
 
-    const theme:ThemeInterface = useContext(ThemeContext);
+    const theme: ThemeInterface = useContext(ThemeContext);
     const isInverse = useIsInverse(isInverseProp);
     const [actions, setActions] = useState(<CloseIcon />);
 
-    const handleRemoveFile = (file: FilePreview ) => {
-      if(onRemoveFile && typeof onRemoveFile === 'function'){
-        onRemoveFile(file)
+    const handleRemoveFile = (file: FilePreview) => {
+      if (onRemoveFile && typeof onRemoveFile === 'function') {
+        onRemoveFile(file);
       }
-    }
+    };
 
-    const handleDeleteFile = (file: FilePreview ) => {
-      if(onDeleteFile && typeof onDeleteFile === 'function'){
-        onDeleteFile(file)
+    const handleDeleteFile = (file: FilePreview) => {
+      if (onDeleteFile && typeof onDeleteFile === 'function') {
+        onDeleteFile(file);
       }
-    }
+    };
 
-    const FinishedActions = ({status='ready'}:{status?:string}) => {
+    const FinishedActions = ({ status = 'ready' }: { status?: string }) => {
       const [done, setDone] = useState(false);
 
       useEffect(() => {
         setTimeout(() => {
           setDone(true);
-        }, 500)
-      }, [status])
+        }, 500);
+      }, [status]);
 
-      if(status ===  'error' || status ===  'ready') {
-        return <StatusIcons>
-          <IconButton
-            onClick={() => {handleRemoveFile(file)}}
-            variant={ButtonVariant.link}
-            color={ButtonColor.secondary}
-            aria-label="Remove File"
-            icon={<CloseIcon />}
-          />
-        </StatusIcons>
+      if (status === 'error' || status === 'ready') {
+        return (
+          <StatusIcons>
+            <IconButton
+              onClick={() => {
+                handleRemoveFile(file);
+              }}
+              variant={ButtonVariant.link}
+              color={ButtonColor.secondary}
+              aria-label="Remove File"
+              icon={<CloseIcon />}
+            />
+          </StatusIcons>
+        );
       }
 
-      if(status === 'pending') {
-        return <StatusIcons>
-          <Spinner/>
-        </StatusIcons>
+      if (status === 'pending') {
+        return (
+          <StatusIcons>
+            <Spinner />
+          </StatusIcons>
+        );
       }
 
-      return <StatusIcons>
-        <Transition isOpen={!done} unmountOnExit nudgeBottom fade>
-          <CheckCircleIcon/>
-        </Transition>
-        <Transition isOpen={done} unmountOnExit nudgeTop fade>
-          <IconButton
-            onClick={() => {handleDeleteFile(file)}}
-            variant={ButtonVariant.link}
-            color={ButtonColor.secondary}
-            aria-label="Delete File"
-            icon={<DeleteIcon />}
-          />
-        </Transition>
-      </StatusIcons>
-    }
+      return (
+        <StatusIcons>
+          <Transition isOpen={!done} unmountOnExit nudgeBottom fade>
+            <CheckCircleIcon />
+          </Transition>
+          <Transition isOpen={done} unmountOnExit nudgeTop fade>
+            <IconButton
+              onClick={() => {
+                handleDeleteFile(file);
+              }}
+              variant={ButtonVariant.link}
+              color={ButtonColor.secondary}
+              aria-label="Delete File"
+              icon={<DeleteIcon />}
+            />
+          </Transition>
+        </StatusIcons>
+      );
+    };
 
     useEffect(() => {
-      setActions(<FinishedActions status={file?.processor?.status}/>);
-    }, [file?.processor?.status])
+      setActions(<FinishedActions status={file?.processor?.status} />);
+    }, [file?.processor?.status]);
 
     return (
       <InverseContext.Provider value={{ isInverse }}>
-        <StyledCard isInverse={isInverse} theme={theme} file={file} data-testid={props.testId} ref={ref}>
+        <StyledCard
+          isInverse={isInverse}
+          theme={theme}
+          file={file}
+          data-testid={props.testId}
+          ref={ref}
+        >
           <StyledFlex
             theme={theme}
             behavior={FlexBehavior.container}
             alignItems={FlexAlignItems.center}
             {...rest}
           >
-            <Flex behavior={FlexBehavior.item} >
-              {
-                file.errors? <ErrorIcon color={isInverse ? theme.colors.dangerInverse : theme.colors.danger} size={24} /> :
-                file.preview && thumbnails && file.type && file.type.startsWith('image')?
-                  <Thumb file={file}/> :
-                  <FileIcon isInverse={isInverse} file={file}/>
-              }
+            <Flex
+              behavior={FlexBehavior.item}
+              alignItems={FlexAlignItems.center}
+            >
+              {file.errors ? (
+                <ErrorIcon
+                  color={
+                    isInverse ? theme.colors.dangerInverse : theme.colors.danger
+                  }
+                  size={24}
+                />
+              ) : file.preview &&
+                thumbnails &&
+                file.type &&
+                file.type.startsWith('image') ? (
+                <Thumb file={file} />
+              ) : (
+                <FileIcon isInverse={isInverse} file={file} />
+              )}
             </Flex>
             <FileName xs behavior={FlexBehavior.item}>
               {file.name}
             </FileName>
-            <Flex style={{marginLeft: 'auto'}} behavior={FlexBehavior.item}>
+            <Flex style={{ marginLeft: 'auto' }} behavior={FlexBehavior.item}>
               {file.processor && file.processor.percent}
             </Flex>
-            <Flex behavior={FlexBehavior.item}>
-              {actions}
-            </Flex>
+            <Flex behavior={FlexBehavior.item}>{actions}</Flex>
           </StyledFlex>
-          {file.errors && <Errors theme={theme}>
-            {file.errors.map(error => <>
-              <ErrorCode>{error.code}</ErrorCode>
-              <ErrorMessage>{error.message}</ErrorMessage>
-            </>)}
-          </Errors>}
+          {file.errors && (
+            <Errors theme={theme}>
+              {file.errors.map(error => (
+                <>
+                  <ErrorCode>{error.code}</ErrorCode>
+                  <ErrorMessage>{error.message}</ErrorMessage>
+                </>
+              ))}
+            </Errors>
+          )}
         </StyledCard>
       </InverseContext.Provider>
     );
   }
-)
+);
