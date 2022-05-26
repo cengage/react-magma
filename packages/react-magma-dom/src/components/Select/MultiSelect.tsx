97ea53aca93ea1669f113916c105af6c254beb07
Laura Silva <91160746+silvalaura@users.noreply.github.com>
2022-05-26 10:10:50 -0400

    feat(combobox/select): update styling for items list inverse (#732)
    * feat(combobox/select): update styling for items list inverse
    
    re #731
    
    * .

--- packages/react-magma-dom/src/components/Select/MultiSelect.tsx
+++ packages/react-magma-dom/src/components/Select/MultiSelect.tsx
@@ -216,6 +216,7 @@ export function MultiSelect<T>(props: MultiSelectProps<T>) {
         getMenuProps={getMenuProps}
         highlightedIndex={highlightedIndex}
         isOpen={isOpen}
+        isInverse={isInverse}
         items={getFilteredItems(items)}
         itemToString={itemToString}
         maxHeight={itemListMaxHeight || theme.select.menu.maxHeight}
