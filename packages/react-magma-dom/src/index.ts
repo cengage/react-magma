9939e8d0d7ae51268a2c63ac61cdb39b7bbfddfc
Chris Cedrone <chris.cedrone@cengage.com>
2022-06-06 13:05:59 -0400

    trying the server end to see if tables show up

--- packages/react-magma-dom/src/index.ts
+++ packages/react-magma-dom/src/index.ts
@@ -114,6 +114,7 @@ export {
 } from './components/InputBase';
 export { InputMessage } from './components/Input/InputMessage';
 export { Label, LabelPosition } from './components/Label';
+export { List, ListProps, ListItemProps } from './components/List';
 export {
   LoadingIndicator,
   LoadingIndicatorProps,
@@ -183,13 +184,9 @@ export {
 export { Tab } from './components/Tabs/Tab';
 export { TabPanelsContainer } from './components/Tabs/TabPanelsContainer';
 export { TabPanel } from './components/Tabs/TabPanel';
-export {
-  Tag,
-  TagProps,
-  BaseTagProps,
-  ClickableTagProps,
-  DeletableTagProps,
-} from './components/Tag';
+
+export { Tag, TagProps } from './components/Tag';
+
 export {
   ToastsContainer,
   ToastsContainerProps,
@@ -240,5 +237,6 @@ export * from './components/Transition';
 export * from './components/Drawer';
 export * from './components/FormFieldContainer';
 export * from './components/Grid';
+export * from './components/Tag';
 export * from './components/List';
 export * from './components/BlockQuote';
