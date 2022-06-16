ed2850aa964cf2c52b1137753b336f4ba227cf52
Chris Cedrone <chris.cedrone@cengage.com>
2022-06-16 15:48:24 -0400

    updating snap and adding storybook example for overflow

--- packages/react-magma-dom/src/components/Alert/Alert.stories.tsx
+++ packages/react-magma-dom/src/components/Alert/Alert.stories.tsx
@@ -9,6 +9,15 @@ const Template: Story<AlertProps> = args => (
   <Alert {...args}>I am an Alert</Alert>
 );
 
+const LongTextTemplate: Story<AlertProps> = args => (
+  <Alert {...args}>
+    Alert Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat.
+  </Alert>
+);
+
 export default {
   title: 'Alert',
   component: Alert,
@@ -29,6 +38,9 @@ export const Dismissible = Template.bind({});
 Dismissible.args = {
   isDismissible: true,
 };
+
+export const LongText = LongTextTemplate.bind({});
+Default.args = {};
 
 export const Danger = Template.bind({});
 Danger.args = {
