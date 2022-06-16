613a4beef9cb4a5ba15370c7382c540a5aefcf74
Chris Cedrone <chris.cedrone@cengage.com>
2022-06-16 15:38:47 -0400

    moving padding to AlertContents

--- packages/react-magma-dom/src/components/AlertBase/index.tsx
+++ packages/react-magma-dom/src/components/AlertBase/index.tsx
@@ -190,7 +190,9 @@ const StyledAlertInner = styled.div<AlertBaseProps>`
 const AlertContents = styled.div`
   align-self: center;
   flex-grow: 1;
-  padding: ${props => props.theme.spaceScale.spacing04} 0;
+  padding: ${props => props.theme.spaceScale.spacing04}
+    ${props => props.theme.spaceScale.spacing04}
+    ${props => props.theme.spaceScale.spacing04} 0;
 
   @media (max-width: ${props => props.theme.breakpoints.small}px) {
     padding-left: ${props => props.theme.spaceScale.spacing04};
