262d7c10aa960cdaea8047dc9432d2fd3fff8d36
Laura Silva <silvalaurav@gmail.com>
2022-07-28 15:11:33 -0400

    fix(datepicker): fix support for non-default date formats (dd/mm/yyyy, yyyy/MM/dd, yyyy/dd/MM)
    fix #295 & #748

--- packages/react-magma-dom/src/components/DatePicker/DatePicker.stories.tsx
+++ packages/react-magma-dom/src/components/DatePicker/DatePicker.stories.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { DatePicker } from '.';
+import { I18nContext } from '../../i18n';
+import { defaultI18n } from '../../i18n/default';
 import { magma } from '../../theme/magma';
-import { Card } from '../Card';
 
 export default {
   component: DatePicker,
@@ -12,6 +13,39 @@ const today: Date = new Date();
 
 export const Default = () => {
   return <DatePicker labelText="Date" minDate={today} />;
+};
+
+export const NonDefaultFormats = () => {
+  return (
+    <>
+      <I18nContext.Provider
+        value={{
+          ...defaultI18n,
+          dateFormat: 'dd/MM/yyyy',
+        }}
+      >
+        <DatePicker labelText="Date format: dd/MM/yyyy" />
+      </I18nContext.Provider>
+      <br />
+      <I18nContext.Provider
+        value={{
+          ...defaultI18n,
+          dateFormat: 'yyyy/MM/dd',
+        }}
+      >
+        <DatePicker labelText="Date format: yyyy/MM/dd" />
+      </I18nContext.Provider>
+      <br />
+      <I18nContext.Provider
+        value={{
+          ...defaultI18n,
+          dateFormat: 'yyyy/dd/MM',
+        }}
+      >
+        <DatePicker labelText="Date format: yyyy/dd/MM" />
+      </I18nContext.Provider>
+    </>
+  );
 };
 
 export const Inverse = () => {
