135f882d10059b8672fafa1a682501d0ef0a1bbf
Laura Silva <silvalaurav@gmail.com>
2022-08-02 15:24:22 -0400

    refactor(radio): update Storybook examples with a11y violations
    fix #514

---
+++ .changeset/radio-storybooka11y.md
@@ -0,0 +1,5 @@
+---
+"react-magma-dom": patch
+---
+
+refactor(radio): Update Storybook examples with a11y violations.
--- packages/react-magma-dom/src/components/Radio/Radio.stories.tsx
+++ packages/react-magma-dom/src/components/Radio/Radio.stories.tsx
@@ -14,12 +14,10 @@ export const Default = () => {
     <>
       <RadioGroup
         labelText="Basic Usage"
-        id="basicGroup"
-        name="basic"
-        value="selectedOption"
+        id="basicGroup1"
+        name="basic1"
       >
         <Radio id="radio1" labelText="Option one label" value="1" />
-
         <Radio
           id="radio2"
           labelText="Option two label is really long and can wrap to multiple lines lorem ipsum dolar sit amet is really long and can wrap to multiple lines"
@@ -34,27 +32,27 @@ export const Default = () => {
       </RadioGroup>
       <RadioGroup
         labelText="Disabled"
-        id="basicGroup"
-        name="basic"
-        value="selectedOption"
+        id="basicGroup2"
+        name="basic2"
+        value="5"
       >
         <Radio id="radio4" disabled labelText="Disabled" value="4" />
         <Radio
           id="radio5"
           disabled
           labelText="Disabled checked"
-          value="selectedOption"
+          value="5"
         />
       </RadioGroup>
       <RadioGroup
         labelText="Error"
-        id="basicGroup"
-        name="basic"
-        value="selectedOption"
+        id="basicGroup3"
+        name="basic3"
+        value="6"
         errorMessage="Error message goes here"
       >
-        <Radio id="radio5" labelText="Error" value="5" />
-        <Radio id="radio6" labelText="Error checked" value="selectedOption" />
+        <Radio id="radio6" labelText="Error checked" value="6" />
+        <Radio id="radio7" labelText="Error" value="7" />
       </RadioGroup>
     </>
   );
@@ -70,16 +68,21 @@ export const Inverse = () => {
             id="inverseGroup"
             isInverse
             name="inverse"
-            value="selectedOption"
+            value="1"
           >
             <Radio id="inverseRadio" labelText="Inverse" value="default" />
-            <Radio id="inverseRadio0" labelText="Inverse with success color" value="0" color={magma.colors.success}/>
+            <Radio
+              id="inverseRadio0"
+              labelText="Inverse with success color"
+              value="0"
+              color={magma.colors.success}
+            />
             <Radio
               isInverse
               disabled
               id="disabledInverseRadio"
               labelText="Disabled inverse"
-              value="selectedOption"
+              value="1"
             />
           </RadioGroup>
         </CardBody>
@@ -92,14 +95,14 @@ export const Inverse = () => {
             isInverse
             name="inverse"
             labelText="Error Inverse Radio Buttons"
-            value="selectedOption"
+            value="3"
           >
-            <Radio id="errorInverseRadio" labelText="Error inverse" value="3" />
+            <Radio id="errorInverseRadio" labelText="Error inverse" value="2" />
             <Radio
               id="errorInverseErrorGroup"
               isInverse
               labelText="Error inverse checked"
-              value="selectedOption"
+              value="3"
             />
           </RadioGroup>
         </CardBody>
