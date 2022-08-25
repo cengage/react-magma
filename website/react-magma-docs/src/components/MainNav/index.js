f0a4487f1e5a579e95a6d10929715a2322cc8a14
Chris Cedrone <chris.cedrone@cengage.com>
2022-08-25 10:37:46 -0400

    initial accordion conversion

--- website/react-magma-docs/src/components/MainNav/index.js
+++ website/react-magma-docs/src/components/MainNav/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, StaticQuery, graphql, withPrefix } from 'gatsby';
 import { Location, Router } from '@reach/router';
-import { ExpandMoreIcon, LaunchIcon } from 'react-magma-icons';
+import { LaunchIcon } from 'react-magma-icons';
 import { SubMenu2 } from './SubMenu';
 import {
   Accordion,
+  AccordionButton,
   AccordionItem,
-  AccordionItemTitle,
-  AccordionItemBody,
-} from 'react-accessible-accordion';
+  AccordionPanel,
+  magma,
+} from 'react-magma-dom';
 import styled from '@emotion/styled';
-import { magma } from 'react-magma-dom';
 
-const StyledAccordion = styled(Accordion)`
+const StyledWrapper = styled.div`
   margin-top: 8px;
   *:focus {
     outline-offset: 0;
@@ -22,34 +22,32 @@ const StyledAccordion = styled(Accordion)`
     margin-top: 4px;
   }
 `;
+const StyledAccordion = styled(Accordion)`
+  border-bottom: 0;
+`;
+
+const StyledAccordionPanel = styled(AccordionPanel)`
+  padding: 0;
+  &[aria-hidden='false'] {
+    box-shadow: inset 0 -1px 0 0 ${magma.colors.neutral300};
+    padding-bottom: 10px;
+  }
+`;
+
 const StyledAccordionItem = styled(AccordionItem)`
-  /* Accordion styles */
-  .accordion__body {
-    animation: fadein 0.35s ease-in;
-    display: block;
-    &[aria-hidden='false'] {
-      box-shadow: inset 0 -1px 0 0 ${magma.colors.neutral300};
-      padding-bottom: 10px;
-    }
+  h3 {
+    color: ${magma.colors.neutral700};
+    font-size: ${magma.typeScale.size01.fontSize};
+    font-weight: 700;
+    text-transform: uppercase;
+    display: inline-block;
+    text-indent: 0;
+    margin: 12px 0;
+    padding-left: ${magma.spaceScale.spacing05};
   }
-  .accordion__body--hidden {
-    animation: fadeout 0.35s ease-in;
-    display: none;
-  }
-  @keyframes fadein {
-    0% {
-      height: 0;
-    }
-    100% {
-      height: 100%;
-    }
-  }
-  @keyframes fadeout {
-    0% {
-      height: 100%;
-    }
-    100% {
-      height: 0;
+  button {
+    &[aria-expanded='true'] {
+      box-shadow: inset 0 1px 0 0 ${magma.colors.neutral300};
     }
   }
 `;
@@ -74,25 +72,11 @@ const LinkHoverStyles = () => `
   background: ${magma.colors.neutral300};
 `;
 
-const StyledAccordionItemTitle = styled(AccordionItemTitle)`
-  cursor: pointer;
-  border: none;
-  svg {
-    transition: transform 0.35s;
-  }
-  &[aria-expanded='true'] {
-    box-shadow: inset 0 1px 0 0 ${magma.colors.neutral300};
-  }
-  &[aria-expanded='true'] svg {
-    transform: rotate(-180deg);
-  }
-`;
-
 const activeStyleDefault = {
   background: magma.colors.neutral300,
 };
 
-const Heading2 = styled.h2`
+const headingStyles = `
   display: flex;
   justify-content: space-between;
   font-size: ${magma.typeScale.size03.fontSize};
@@ -103,6 +87,15 @@ const Heading2 = styled.h2`
   &:hover {
     ${LinkHoverStyles};
   }
+`;
+
+const Heading2 = styled.h2`
+  ${headingStyles};
+`;
+
+const StyledAccordionButton = styled(AccordionButton)`
+  border-top: 0;
+  ${headingStyles};
 `;
 
 const Heading3 = styled.h3`
@@ -261,49 +254,44 @@ export const MainNav = ({ ...props }) => {
         }
       `}
       render={data => (
-        <>
+        <StyledWrapper>
+          <StyledLink
+            activeStyle={activeStyle}
+            aria-label="Introduction to the Magma System"
+            onClick={props.handleClick}
+            to="/"
+          >
+            <Heading2>Introduction</Heading2>
+          </StyledLink>
+
+          <StyledLink
+            activeStyle={activeStyle}
+            aria-label="Contribution Guidelines"
+            to="/contribution-guidelines/"
+          >
+            <Heading2>Contribution Guidelines</Heading2>
+          </StyledLink>
+
+          <StyledLink
+            activeStyle={activeStyle}
+            aria-label="View project on GitHub"
+            href="https://github.com/cengage/react-magma"
+          >
+            <Heading2>
+              GitHub
+              <LaunchIcon size={magma.iconSizes.small} />
+            </Heading2>
+          </StyledLink>
           <Location>
             {({ location }) => (
-              <StyledAccordion accordion={false}>
-                <StyledAccordionItem>
-                  <StyledLink
-                    activeStyle={activeStyle}
-                    aria-label="Introduction to the Magma System"
-                    onClick={props.handleClick}
-                    to="/"
-                  >
-                    <Heading2>Introduction</Heading2>
-                  </StyledLink>
-
-                  <StyledLink
-                    activeStyle={activeStyle}
-                    aria-label="Contribution Guidelines"
-                    to="/contribution-guidelines/"
-                  >
-                    <Heading2>Contribution Guidelines</Heading2>
-                  </StyledLink>
-
-                  <StyledLink
-                    activeStyle={activeStyle}
-                    aria-label="View project on GitHub"
-                    href="https://github.com/cengage/react-magma"
-                  >
-                    <Heading2>
-                      GitHub
-                      <LaunchIcon size={magma.iconSizes.small} />
-                    </Heading2>
-                  </StyledLink>
-                </StyledAccordionItem>
+              <StyledAccordion>
                 <StyledAccordionItem
-                  expanded={location.pathname.includes('design')}
+                  value={location.pathname.includes('design')}
                 >
-                  <StyledAccordionItemTitle>
-                    <Heading2>
-                      Design
-                      <ExpandMoreIcon size={magma.iconSizes.medium} />
-                    </Heading2>
-                  </StyledAccordionItemTitle>
-                  <AccordionItemBody>
+                  <StyledAccordionButton>
+                    <Heading2>Design</Heading2>
+                  </StyledAccordionButton>
+                  <StyledAccordionPanel>
                     <List>
                       <Heading3>Intro</Heading3>
                       {data.designIntro.edges.map(({ node }) => (
@@ -367,19 +355,14 @@ export const MainNav = ({ ...props }) => {
                         </ListItem>
                       ))}
                     </List>
-                  </AccordionItemBody>
+                  </StyledAccordionPanel>
                 </StyledAccordionItem>
 
-                <StyledAccordionItem
-                  expanded={location.pathname.includes('api')}
-                >
-                  <StyledAccordionItemTitle>
-                    <Heading2>
-                      Components
-                      <ExpandMoreIcon size={magma.iconSizes.medium} />
-                    </Heading2>
-                  </StyledAccordionItemTitle>
-                  <AccordionItemBody>
+                <StyledAccordionItem value={location.pathname.includes('api')}>
+                  <StyledAccordionButton>
+                    <Heading2>Components</Heading2>
+                  </StyledAccordionButton>
+                  <StyledAccordionPanel>
                     <Heading3>Intro</Heading3>
                     <List>
                       {data.apiIntro.edges.map(({ node }) => (
@@ -422,19 +405,16 @@ export const MainNav = ({ ...props }) => {
                         </ListItem>
                       ))}
                     </List>
-                  </AccordionItemBody>
+                  </StyledAccordionPanel>
                 </StyledAccordionItem>
 
                 <StyledAccordionItem
-                  expanded={location.pathname.includes('patterns')}
+                  value={location.pathname.includes('patterns')}
                 >
-                  <StyledAccordionItemTitle>
-                    <Heading2>
-                      Patterns
-                      <ExpandMoreIcon size={magma.iconSizes.medium} />
-                    </Heading2>
-                  </StyledAccordionItemTitle>
-                  <AccordionItemBody>
+                  <StyledAccordionButton>
+                    <Heading2>Patterns</Heading2>
+                  </StyledAccordionButton>
+                  <StyledAccordionPanel>
                     <Heading3>Intro</Heading3>
                     <List>
                       {data.patternsIntro.edges.map(({ node }) => (
@@ -477,12 +457,12 @@ export const MainNav = ({ ...props }) => {
                         </ListItem>
                       ))}
                     </List>
-                  </AccordionItemBody>
+                  </StyledAccordionPanel>
                 </StyledAccordionItem>
               </StyledAccordion>
             )}
           </Location>
-        </>
+        </StyledWrapper>
       )}
     />
   );
