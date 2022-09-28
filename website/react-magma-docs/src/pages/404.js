c9781dd7de67b7d18819bbed26e131fadd0768c7
Laura Silva <silvalaurav@gmail.com>
2022-09-28 13:39:32 -0400

    remove extra netlify button

--- website/react-magma-docs/src/pages/404.js
+++ website/react-magma-docs/src/pages/404.js
@@ -7,30 +7,28 @@ import {
   Heading,
   Hyperlink,
 } from 'react-magma-dom';
-import { Layout } from '../components/layout';
+import { LayoutComponent } from '../components/LayoutComponent';
 
 const NotFoundPage = () => (
-  <Layout>
-    <>
-      <Heading level={1}>Sorry, page not found.</Heading>
-      <p>
-        The page you were looking for may have been moved, removed, renamed or
-        might never have existed.
-      </p>
-      <ButtonGroup>
-        <Button onClick={() => navigate(-1)} color={ButtonColor.secondary}>
-          Back to previous page
-        </Button>
-        <Hyperlink styledAs="Button" to="/" color={ButtonColor.primary}>
-          {linkProps => (
-            <Link {...linkProps}>
-              <span>Go to home page</span>
-            </Link>
-          )}
-        </Hyperlink>
-      </ButtonGroup>
-    </>
-  </Layout>
+  <LayoutComponent>
+    <Heading level={1}>Sorry, page not found.</Heading>
+    <p>
+      The page you were looking for may have been moved, removed, renamed or
+      might never have existed.
+    </p>
+    <ButtonGroup>
+      <Button onClick={() => navigate(-1)} color={ButtonColor.secondary}>
+        Back to previous page
+      </Button>
+      <Hyperlink styledAs="Button" to="/" color={ButtonColor.primary}>
+        {linkProps => (
+          <Link {...linkProps}>
+            <span>Go to home page</span>
+          </Link>
+        )}
+      </Hyperlink>
+    </ButtonGroup>
+  </LayoutComponent>
 );
 
 export default NotFoundPage;
