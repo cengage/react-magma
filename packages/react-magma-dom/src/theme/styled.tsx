f4091c0acba3ecb97e0ec9f877f9fe03a83ec297
Laura Silva <silvalaurav@gmail.com>
2022-12-14 14:47:25 -0500

    cleanup

--- packages/react-magma-dom/src/theme/styled.tsx
+++ packages/react-magma-dom/src/theme/styled.tsx
@@ -2,13 +2,3 @@ import styled, { CreateStyled } from '@emotion/styled';
 import { ThemeInterface } from './magma';
 
 export default styled as CreateStyled<ThemeInterface>;
-
-
-// declare module '@emotion/styled' {
-//     import { CreateStyled } from '@emotion/styled/types/index'
-//     import { ThemeInterface } from './magma'
-  
-//     export * from '@emotion/styled/types/index'
-//     const customStyled: CreateStyled<ThemeInterface>
-//     export default customStyled
-//   }
