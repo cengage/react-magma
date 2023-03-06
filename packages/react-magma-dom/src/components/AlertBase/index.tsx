e5e863b10a6419d996a9b57ef5c5ba19b45527b4
Michael Murray <mcmurray0125@gmail.com>
2023-03-06 14:22:17 -0600

    fix(alertbase): added height variable to StyledAlertInner, isToast. Added changeset
    fix #1010

--- packages/react-magma-dom/src/components/AlertBase/index.tsx
+++ packages/react-magma-dom/src/components/AlertBase/index.tsx
@@ -266,7 +266,6 @@ const StyledAlertInner = styled.div<AlertBaseProps>`
   color: ${buildAlertColor};
   display: flex;
   position: relative;
-  height: 56px;
 
   ${props =>
     props.isToast &&
@@ -275,6 +274,7 @@ const StyledAlertInner = styled.div<AlertBaseProps>`
         ? `0 2px 8px 0 ${transparentize(0.3, props.theme.colors.neutral900)}`
         : `0 2px 8px 0 ${transparentize(0.6, props.theme.colors.neutral900)}`};
       padding-right: 0;
+      height: ${props.theme.spaceScale.spacing11};
     `}
 `;
 
