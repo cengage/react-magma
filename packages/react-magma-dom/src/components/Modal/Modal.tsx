81bed06d4c612cf2440370a1d85cfec14cc45153
Laura Silva <91160746+silvalaura@users.noreply.github.com>
2023-09-27 09:26:40 -0400

    fix(Modal): Update padding so it's equal on all sides (#1138)

--- packages/react-magma-dom/src/components/Modal/Modal.tsx
+++ packages/react-magma-dom/src/components/Modal/Modal.tsx
@@ -401,7 +401,7 @@ export const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
                 theme={theme}
               >
                 {header && (
-                  <ModalHeader theme={theme}>
+                  <ModalWrapper theme={theme}>
                     {header && (
                       <H1
                         id={headingId}
@@ -415,7 +415,7 @@ export const Modal = React.forwardRef<HTMLDivElement, ModalProps>(
                         {header}
                       </H1>
                     )}
-                  </ModalHeader>
+                  </ModalWrapper>
                 )}
                 <ModalWrapper ref={bodyRef} theme={theme}>
                   {children}
