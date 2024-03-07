f9f339968ad982daa9df84c7f5e1016a64307839
Laura Silva <silvalaurav@gmail.com>
2024-03-07 17:33:12 -0500

    feat(TreeView): Updating selectedItems structure

--- packages/react-magma-dom/src/components/TreeView/TreeViewContext.ts
+++ packages/react-magma-dom/src/components/TreeView/TreeViewContext.ts
@@ -4,14 +4,14 @@ import { TreeViewSelectable } from './useTreeView';
 export interface TreeViewContextInterface {
   children?: React.ReactNode | React.ReactNode[];
   hasIcons: boolean;
-  onSelectedItemChange?: (selectedItems: any) => void;
+  onSelectedItemChange?: (selectedItems: Array<Object>) => void;
   onExpandedChange?: (event: React.SyntheticEvent) => void;
   selectable: TreeViewSelectable;
   setHasIcons: React.Dispatch<React.SetStateAction<boolean>>;
-  selectedItems: Array<string>;
+  selectedItems: Array<Object>;
   setSelectedItems: React.Dispatch<React.SetStateAction<any>>;
   initialExpandedItems: Array<string>;
-  initialSelectedItems: Array<string>;
+  initialSelectedItems: Array<Object>;
   treeItemRefArray?: React.MutableRefObject<React.MutableRefObject<Element>[]>;
   registerTreeItem: (
     itemRefArray: React.MutableRefObject<React.MutableRefObject<Element>[]>,
