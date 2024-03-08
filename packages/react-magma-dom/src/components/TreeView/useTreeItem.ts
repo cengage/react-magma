be48337cc400779340f1d6ff7596c558085c3a2c
Laura Silva <91160746+silvalaura@users.noreply.github.com>
2024-03-07 21:13:40 -0500

    feat(TreeView): Updating selectedItems structure (#1205)

--- packages/react-magma-dom/src/components/TreeView/useTreeItem.ts
+++ packages/react-magma-dom/src/components/TreeView/useTreeItem.ts
@@ -483,12 +483,20 @@ export function useTreeItem(props: UseTreeItemProps, forwardedRef) {
         statusFromChildren
       );
 
+      const updateItemStatus = getUpdatedSelectedItems(
+        selectedItems,
+        itemId,
+        statusFromChildren
+      );
+
       if (
         checkedStatus !== statusFromChildren &&
         statusUpdatedBy !== StatusUpdatedByOptions.parent
       ) {
         setStatusUpdatedBy(StatusUpdatedByOptions.children);
         setCheckedStatus(statusFromChildren);
+        setSelectedItems(updateItemStatus);
+
         if (
           statusFromChildren === IndeterminateCheckboxStatus.checked ||
           statusFromChildren === IndeterminateCheckboxStatus.indeterminate
@@ -544,6 +552,8 @@ export function useTreeItem(props: UseTreeItemProps, forwardedRef) {
         if (!expanded) {
           updateSelectedItemsChanged();
         }
+      } else {
+        setSelectedItems(updateItemStatus);
       }
     }
   }, [childrenCheckedStatus]);
@@ -623,6 +633,7 @@ export function useTreeItem(props: UseTreeItemProps, forwardedRef) {
           childrenIds,
           { itemId, checkedStatus }
         );
+
         setSelectedItems(newSelectedItems);
         updateSelectedItemsChanged();
       }
