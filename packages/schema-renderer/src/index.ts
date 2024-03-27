f5a77bf8c35bc1ca126f6371847c26e46dd27903
Laura Silva <silvalaurav@gmail.com>
2024-03-27 15:06:02 -0400

    .

--- packages/schema-renderer/src/index.ts
+++ packages/schema-renderer/src/index.ts
@@ -1,4 +1,4 @@
 export * from './components/SchemaRenderer';
-export * from './components/ComponentMapper';
+// export * from './components/ComponentMapper';
 export * from './components/TemplateMapper';
 export * from './components/ValidatorMapper';
