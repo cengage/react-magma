4d53c322ef921ad4cb47461bcb56771bb9e0dc29
Laura Silva <silvalaurav@gmail.com>
2024-03-29 15:37:08 -0400

    .

--- packages/react-magma-dom/babel.config.js
+++ packages/react-magma-dom/babel.config.js
@@ -12,7 +12,7 @@ module.exports = function(api) {
               default: {
                 canonicalImport: ['@emotion/styled', 'default'],
                 styledBaseImport: [
-                  '@emotion/styled/base/dist/emotion-styled-base.esm.js',
+                  '@emotion/styled/dist/styled-base.esm.js',
                   'default',
                 ],
               },
