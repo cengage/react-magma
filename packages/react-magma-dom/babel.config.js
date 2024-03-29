3ef0dc24f5819d63748d6ecccc2febc77688eab9
Laura Silva <silvalaurav@gmail.com>
2024-03-29 17:01:15 -0400

    another try with babel config

--- packages/react-magma-dom/babel.config.js
+++ packages/react-magma-dom/babel.config.js
@@ -1,8 +1,15 @@
-const babelConfig = require('../../babel.config')
+const babelConfig = require('../../babel.config');
 
-module.exports = function(api) {
+const path = require('path');
+const emotionPkg = require('@emotion/styled/base/package.json');
+const styledBaseImport = [
+  '@emotion/styled/base/' + path.normalize(emotionPkg.module),
+  'default',
+];
+
+module.exports = function (api) {
   return {
-    ...babelConfig(api), 
+    ...babelConfig(api),
     plugins: [
       [
         '@emotion',
@@ -10,12 +17,30 @@ module.exports = function(api) {
           importMap: {
             '@emotion/styled': {
               default: {
-                canonicalImport: ['@emotion/styled', 'default']
+                canonicalImport: ['@emotion/styled', 'default'],
+                styledBaseImport,
+              },
+            },
+          },
+        },
+      ],
+      [
+        '@emotion/babel-preset-css-prop',
+        {
+          autoLabel: 'always',
+          labelFormat: '[local]',
+          useBuiltIns: false,
+          throwIfNamespace: true,
+          importMap: {
+            '@emotion/styled': {
+              default: {
+                canonicalImport: ['@emotion/styled', 'default'],
+                styledBaseImport,
               },
             },
           },
         },
       ],
     ],
-  }
-}
+  };
+};
