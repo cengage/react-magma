fdbf38ab0b27e5a16d11a1fbe1539e942f0f05ca
Vladislav Verein <vladislav.verein@cengage.com>
2024-04-19 14:34:22 +0200

    Revert "Fix issue with 'ReferenceError: _styled is not defined' (#1216)"
    This reverts commit f7ee4a77fed33fe5e4bb851cf80c9053c71bf834.

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,5 @@
-import styled from '@emotion/styled/';
-
 const emotion = require('@emotion/core');
 const { createSerializer, matchers } = require('jest-emotion');
-
-global._styled = styled;
 
 expect.addSnapshotSerializer(createSerializer(emotion));
 expect.extend(matchers);
