e38ee20e1eef32a3d70d0b6e7fc1c17cdbfe5080
ccedrone <77400920+ccedrone@users.noreply.github.com>
2024-07-03 12:19:01 -0400

    feat(Stepper): Displays step based content for use in multi-step interfaces. (#1292)
    Co-authored-by: Orion Johnson <orion.johnson@cengage.com>
    Co-authored-by: Chris Cedrone <ccedrone@mabosccedrone-m1.hsd1.ma.comcast.net>

---
+++ packages/react-magma-dom/src/components/Stepper/Step.tsx
@@ -0,0 +1,266 @@
+import * as React from 'react';
+import { CheckIcon, CrossIcon } from 'react-magma-icons';
+import styled from '@emotion/styled';
+import { CreateStyled } from '@emotion/styled';
+
+import { ThemeContext } from '../../theme/ThemeContext';
+import { ThemeInterface } from '../../theme/magma';
+import { useIsInverse } from '../../inverse';
+import { transparentize } from 'polished';
+import { HiddenStyles } from '../../utils/UtilityStyles';
+
+export interface StepProps extends React.HTMLAttributes<HTMLDivElement> {
+  /**
+   * Error state for each step.
+   * @default false
+   */
+  hasError?: boolean;
+  /**
+   * @internal
+   */
+  areLabelsHidden?: boolean;
+  /**
+   * Label beneath each step.
+   */
+  label?: string;
+  /**
+   * Sub label beneath each step.
+   */
+  secondaryLabel?: string;
+  /**
+   * @internal
+   */
+  stepStatus?: StepStatus;
+  /**
+   * @internal
+   */
+  isInverse?: boolean;
+  /**
+   * @internal
+   */
+  testId?: string;
+  /**
+   * @internal
+   */
+  theme?: ThemeInterface;
+}
+
+export enum StepStatus {
+  active = 'active',
+  complete = 'complete',
+  incomplete = 'incomplete',
+}
+
+function buildStepCircleOutlineColors(props) {
+  const { isInverse, stepStatus, hasError, theme } = props;
+
+  if (isInverse && !hasError) {
+    if (stepStatus === StepStatus.active) {
+      return theme.colors.tertiary500;
+    } else if (stepStatus === StepStatus.incomplete) {
+      return theme.colors.primary400;
+    }
+  } else {
+    if (!isInverse && !hasError) {
+      if (stepStatus === StepStatus.active) {
+        return theme.colors.primary500;
+      } else if (stepStatus === StepStatus.incomplete) {
+        return theme.colors.neutral300;
+      }
+    }
+  }
+}
+
+function buildStepCircleBackgroundColors(props) {
+  const { isInverse, stepStatus, hasError, theme } = props;
+  if (isInverse) {
+    if (stepStatus === StepStatus.complete && !hasError) {
+      return theme.colors.tertiary500;
+    } else if (hasError) {
+      return theme.colors.danger500;
+    }
+  } else {
+    if (stepStatus === StepStatus.complete && !hasError) {
+      return theme.colors.primary500;
+    } else if (hasError) {
+      return theme.colors.danger500;
+    }
+  }
+}
+
+function buildStepLabelColors(props) {
+  const { label, secondaryLabel, theme, isInverse } = props;
+
+  if (isInverse) {
+    if (label) {
+      return theme.colors.neutral100;
+    } else if (secondaryLabel) {
+      return transparentize(0.3, theme.colors.neutral100);
+    }
+  } else {
+    if (label) {
+      return theme.colors.neutral700;
+    } else if (secondaryLabel) {
+      return theme.colors.neutral500;
+    }
+  }
+}
+
+function buildStepSvgColors(props) {
+  const { theme, hasError, isInverse } = props;
+  if (isInverse) {
+    if (hasError) {
+      return theme.colors.neutral100;
+    }
+    return theme.colors.primary600;
+  } else {
+    return theme.colors.neutral100;
+  }
+}
+
+const typedStyled = styled as CreateStyled<ThemeInterface>;
+
+export const HiddenLabelText = typedStyled.span`
+  ${HiddenStyles};
+`;
+
+const StyledStep = typedStyled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  align-self: self-start;
+  align-items: center;
+
+`;
+
+const StyledStepIndicator = typedStyled.span<{
+  hasError?: boolean;
+  stepStatus?: StepStatus;
+  isInverse?: boolean;
+  theme?: ThemeInterface;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  width: 24px;
+  height: 24px;
+  box-shadow: inset 0 0 0 2px
+    ${props =>
+      (props.stepStatus === StepStatus.incomplete && !props.hasError) ||
+      (props.stepStatus === StepStatus.active && !props.hasError)
+        ? buildStepCircleOutlineColors
+        : 'none'};
+  background: ${buildStepCircleBackgroundColors};
+  svg {
+    color: ${buildStepSvgColors};
+    width: ${props => props.theme.iconSizes.xSmall}px;
+    height: ${props => props.theme.iconSizes.xSmall}px;
+  }
+`;
+
+const StyledStepTextWrapper = typedStyled.div`
+  flex: 1;
+  display: flex;
+  align-self: center;
+  flex-direction: column;
+  position: relative;
+  margin: 6px 8px 0;
+`;
+
+const StyledLabel = typedStyled.span<{
+  isInverse?: boolean;
+  label?: string;
+  theme?: ThemeInterface;
+}>`
+  color: ${buildStepLabelColors};
+  font-weight: 600;
+  font-size: ${props =>
+    props.theme.typographyVisualStyles.bodySmall.desktop.fontSize};
+  letter-spacing: ${props =>
+    props.theme.typographyVisualStyles.bodySmall.desktop.letterSpacing};
+  line-height: ${props =>
+    props.theme.typographyVisualStyles.bodySmall.desktop.lineHeight};
+`;
+
+const StyledSecondaryLabel = typedStyled.span<{
+  isInverse?: boolean;
+  secondaryLabel?: string;
+  theme?: ThemeInterface;
+}>`
+  color: ${buildStepLabelColors};
+  font-size: ${props =>
+    props.theme.typographyVisualStyles.bodyXSmall.desktop.fontSize};
+  letter-spacing: ${props =>
+    props.theme.typographyVisualStyles.bodyXSmall.desktop.letterSpacing};
+  line-height: ${props =>
+    props.theme.typographyVisualStyles.bodyXSmall.desktop.lineHeight};
+  margin: 2px 12px 0 12px;
+`;
+
+export const Step = React.forwardRef<HTMLDivElement, StepProps>(
+  (props, ref) => {
+    const {
+      hasError,
+      areLabelsHidden,
+      label,
+      secondaryLabel,
+      testId,
+      isInverse: isInverseProp,
+      stepStatus,
+      ...rest
+    } = props;
+    const theme = React.useContext(ThemeContext);
+    const isInverse = useIsInverse(isInverseProp);
+
+    return (
+      <StyledStep {...rest} ref={ref} data-testid={testId}>
+        <StyledStepIndicator
+          hasError={hasError}
+          isInverse={isInverse}
+          stepStatus={stepStatus}
+          theme={theme}
+        >
+          {stepStatus === StepStatus.complete && !hasError && (
+            <CheckIcon aria-hidden="true" />
+          )}
+          {hasError && <CrossIcon aria-hidden="true" />}
+        </StyledStepIndicator>
+
+        <StyledStepTextWrapper>
+          {!areLabelsHidden ? (
+            <>
+              {label && (
+                <StyledLabel
+                  label={label}
+                  isInverse={isInverse}
+                  data-testid={testId && `${testId}-label`}
+                  theme={theme}
+                >
+                  {label}
+                </StyledLabel>
+              )}
+              {secondaryLabel && (
+                <StyledSecondaryLabel
+                  secondaryLabel={secondaryLabel}
+                  isInverse={isInverse}
+                  data-testid={testId && `${testId}-secondaryLabel`}
+                  theme={theme}
+                >
+                  {secondaryLabel}
+                </StyledSecondaryLabel>
+              )}
+            </>
+          ) : (
+            <HiddenLabelText>
+              {label && secondaryLabel
+                ? `${label} ${secondaryLabel}`
+                : label || secondaryLabel}
+            </HiddenLabelText>
+          )}
+        </StyledStepTextWrapper>
+      </StyledStep>
+    );
+  }
+);
---
+++ packages/react-magma-dom/src/components/Stepper/Stepper.test.js
@@ -0,0 +1,514 @@
+import React from 'react';
+import { act } from 'react-dom/test-utils';
+import { axe } from '../../../axe-helper';
+import { magma } from '../../theme/magma';
+import { Stepper, Step, StepperLayout } from '.';
+import { getByTestId, render } from '@testing-library/react';
+import { transparentize } from 'polished';
+import { I18nContext } from '../../i18n';
+import { defaultI18n } from '../../i18n/default';
+
+const TEXT = 'Test Text';
+const testId = 'test-id';
+
+describe('Stepper', () => {
+  it('should find element by testId', () => {
+    const { getByTestId } = render(<Stepper ariaLabel="progress"  testId={testId}></Stepper>);
+
+    expect(getByTestId(testId)).toBeInTheDocument();
+  });
+
+  it('Does not violate accessibility standards', () => {
+    const { container } = render(<Stepper ariaLabel="progress" role="form"></Stepper>);
+
+    return axe(container.innerHTML).then(result => {
+      return expect(result).toHaveNoViolations();
+    });
+  });
+
+  it('should use the custom aria-label', () => {
+    const { getByLabelText } = render(
+      <Stepper ariaLabel="custom" testId={testId}  currentStep={0}>
+        <Step />
+        <Step />
+      </Stepper>
+    );
+
+    const customAriaLabel = getByLabelText('custom', {selector: 'div'});
+
+    expect(customAriaLabel).toBeInTheDocument();
+  });
+
+  it('should use the default completion label', () => {
+    const { getByText } = render(
+      <Stepper ariaLabel="progress"  currentStep={2} layout={StepperLayout.summaryView}>
+        <Step />
+        <Step />
+      </Stepper>
+    );
+
+    expect(getByText('All steps completed')).toBeInTheDocument();
+  });
+
+  it('should use the default step label', () => {
+    const { getByText } = render(
+      <Stepper ariaLabel="progress"  currentStep={0} layout={StepperLayout.summaryView}>
+        <Step />
+        <Step />
+      </Stepper>
+    );
+
+    expect(getByText('Step 1 of 2')).toBeInTheDocument();
+  });
+
+  it('should use the custom completion label', () => {
+    const { getByText } = render(
+      <Stepper ariaLabel="progress" 
+        completionLabel="You're victorious"
+        currentStep={2}
+        layout={StepperLayout.summaryView}
+      >
+        <Step />
+        <Step />
+      </Stepper>
+    );
+
+    expect(getByText("You're victorious")).toBeInTheDocument();
+  });
+
+  it('should use the custom step label', () => {
+    const { getByText } = render(
+      <Stepper ariaLabel="progress" 
+        stepLabel="Party On"
+        currentStep={0}
+        layout={StepperLayout.summaryView}
+      >
+        <Step />
+        <Step />
+      </Stepper>
+    );
+
+    expect(getByText('Party On 1 of 2')).toBeInTheDocument();
+  });
+
+  describe('i18n', () => {
+    it('should use the completion label', () => {
+      const completionLabel = 'completion label';
+      const { getByText } = render(
+        <I18nContext.Provider
+          value={{
+            ...defaultI18n,
+            stepper: {
+              completionLabel,
+            },
+          }}
+        >
+          <Stepper ariaLabel="progress"  currentStep={2} layout={StepperLayout.summaryView}>
+            <Step />
+            <Step />
+          </Stepper>
+        </I18nContext.Provider>
+      );
+
+      expect(getByText(completionLabel)).toHaveTextContent(completionLabel);
+    });
+
+    it('should use the step label', () => {
+      const stepLabel = 'Steppin';
+      const stepOfLabel = 'of';
+      const { getByText } = render(
+        <I18nContext.Provider
+          value={{
+            ...defaultI18n,
+            stepper: {
+              stepLabel,
+              stepOfLabel,
+            },
+          }}
+        >
+          <Stepper ariaLabel="progress"  currentStep={0} layout={StepperLayout.summaryView}>
+            <Step />
+            <Step />
+          </Stepper>
+        </I18nContext.Provider>
+      );
+
+      expect(getByText(stepLabel + ' 1 of 2')).toBeInTheDocument();
+    });
+  });
+
+  describe('Styling', () => {
+    it('should have an active styled circle', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={0}>
+          <Step testId={testId} />
+          <Step />
+        </Stepper>
+      );
+
+      const step = getByTestId(testId).querySelector('span');
+
+      expect(step).toHaveStyleRule(
+        'box-shadow',
+        `inset 0 0 0 2px ${magma.colors.primary500}`
+      );
+    });
+
+    it('should have an incomplete styled circle', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={0}>
+          <Step />
+          <Step testId={testId} />
+        </Stepper>
+      );
+
+      const step = getByTestId(testId).querySelector('span');
+
+      expect(step).toHaveStyleRule(
+        'box-shadow',
+        `inset 0 0 0 2px ${magma.colors.neutral300}`
+      );
+    });
+
+    it('should have a completed styled circle', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={1}>
+          <Step testId={testId} />
+          <Step />
+        </Stepper>
+      );
+
+      const step = getByTestId(testId).querySelector('span');
+
+      expect(step).toHaveStyleRule('background', magma.colors.primary500);
+    });
+
+    it('should have a error styled circle', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={1}>
+          <Step testId={testId} hasError />
+          <Step />
+        </Stepper>
+      );
+
+      const step = getByTestId(testId).querySelector('span');
+
+      expect(step).toHaveStyleRule('background', magma.colors.danger500);
+    });
+
+    it('should have a primary label', () => {
+      const { getByText } = render(
+        <Stepper ariaLabel="progress"  currentStep={1}>
+          <Step label={TEXT} />
+          <Step />
+        </Stepper>
+      );
+
+      const label = getByText(TEXT);
+
+      expect(label).toHaveStyleRule('font-size', '14px');
+      expect(label).toHaveStyleRule('color', magma.colors.neutral700);
+      expect(label).toHaveStyleRule('font-weight', '600');
+    });
+
+    it('should have a secondary label', () => {
+      const { getByText } = render(
+        <Stepper ariaLabel="progress"  currentStep={1}>
+          <Step secondaryLabel={TEXT} hasError />
+          <Step />
+        </Stepper>
+      );
+
+      const secondaryLabel = getByText(TEXT);
+
+      expect(secondaryLabel).toHaveStyleRule('font-size', '12px');
+      expect(secondaryLabel).toHaveStyleRule('color', magma.colors.neutral500);
+    });
+
+    it('should have an incomplete styled separator', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={0}>
+          <Step testId={testId} />
+          <Step />
+        </Stepper>
+      );
+
+      const separator = getByTestId(testId).nextElementSibling;
+
+      expect(separator).toHaveStyleRule('height', '2px');
+      expect(separator).toHaveStyleRule('background', magma.colors.neutral300);
+    });
+
+    it('should have a completed styled separator', () => {
+      const { getByTestId } = render(
+        <Stepper ariaLabel="progress"  currentStep={1}>
+          <Step testId={testId} />
+          <Step />
+        </Stepper>
+      );
+
+      const separator = getByTestId(testId).nextElementSibling;
+
+      expect(separator).toHaveStyleRule('height', '2px');
+      expect(separator).toHaveStyleRule('background', magma.colors.primary500);
+    });
+
+    describe('Inverse', () => {
+      it('should have an inverse active styled circle', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  testId={testId} isInverse currentStep={0}>
+            <Step />
+            <Step />
+          </Stepper>
+        );
+
+        const step = getByTestId(testId).querySelector('span');
+
+        expect(step).toHaveStyleRule(
+          'box-shadow',
+          `inset 0 0 0 2px ${magma.colors.tertiary500}`
+        );
+      });
+
+      it('should have an inverse incomplete styled circle', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={0}>
+            <Step />
+            <Step testId={testId} />
+          </Stepper>
+        );
+
+        const step = getByTestId(testId).querySelector('span');
+
+        expect(step).toHaveStyleRule(
+          'box-shadow',
+          `inset 0 0 0 2px ${magma.colors.primary400}`
+        );
+      });
+
+      it('should have an inverse completed styled circle', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={1}>
+            <Step testId={testId} />
+            <Step />
+          </Stepper>
+        );
+
+        const step = getByTestId(testId).querySelector('span');
+
+        expect(step).toHaveStyleRule('background', magma.colors.tertiary500);
+      });
+
+      it('should have an inverse error styled circle', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={1}>
+            <Step testId={testId} hasError />
+            <Step />
+          </Stepper>
+        );
+
+        const step = getByTestId(testId).querySelector('span');
+
+        expect(step).toHaveStyleRule('background', magma.colors.danger500);
+      });
+
+      it('should have an inverse primary label', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={1}>
+            <Step label={TEXT} hasError />
+            <Step />
+          </Stepper>
+        );
+
+        const label = getByText(TEXT);
+
+        expect(label).toHaveStyleRule('color', magma.colors.neutral100);
+      });
+
+      it('should have an inverse secondary label', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={1}>
+            <Step secondaryLabel={TEXT} hasError />
+            <Step />
+          </Stepper>
+        );
+
+        const secondaryLabel = getByText(TEXT);
+
+        expect(secondaryLabel).toHaveStyleRule(
+          'color',
+          transparentize(0.3, magma.colors.neutral100)
+        );
+      });
+
+      it('should have an inverse incomplete styled separator', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={0}>
+            <Step testId={testId} />
+            <Step />
+          </Stepper>
+        );
+
+        const separator = getByTestId(testId).nextElementSibling;
+
+        expect(separator).toHaveStyleRule(
+          'background',
+          magma.colors.primary400
+        );
+      });
+
+      it('should have an inverse completed styled separator', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress"  isInverse currentStep={1}>
+            <Step testId={testId} />
+            <Step />
+          </Stepper>
+        );
+
+        const separator = getByTestId(testId).nextElementSibling;
+
+        expect(separator).toHaveStyleRule(
+          'background',
+          magma.colors.tertiary500
+        );
+      });
+    });
+
+    describe('States', () => {
+      it('should hide labels when layout is set to "hideLabels"', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress"  layout={StepperLayout.hideLabels} currentStep={1}>
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+
+        const label1 = getByText(`${TEXT}-1`);
+        const label2 = getByText(`${TEXT}-2`);
+
+        expect(label1).toHaveStyleRule('height', '1px');
+        expect(label1).toHaveStyleRule('position', 'absolute');
+        expect(label1).toHaveStyleRule('overflow', 'hidden');
+        expect(label1).toHaveStyleRule('top', 'auto');
+        expect(label1).toHaveStyleRule('white-space', 'nowrap');
+        expect(label1).toHaveStyleRule('width', '1px');
+
+        expect(label2).toHaveStyleRule('height', '1px');
+        expect(label2).toHaveStyleRule('position', 'absolute');
+        expect(label2).toHaveStyleRule('overflow', 'hidden');
+        expect(label2).toHaveStyleRule('top', 'auto');
+        expect(label2).toHaveStyleRule('white-space', 'nowrap');
+        expect(label2).toHaveStyleRule('width', '1px');
+      });
+
+      it('should only show one step label and description at a time along with a counter when layout is set to summaryView', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress" 
+            testId={testId}
+            layout={StepperLayout.summaryView}
+            currentStep={0}
+          >
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+
+        expect(getByTestId(`${testId}-stepper-summary`)).toBeVisible();
+        expect(getByTestId(testId)).toHaveTextContent('Step 1 of 2');
+      });
+
+      it('should show labels when layout is set to showLabels', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress"  layout={StepperLayout.showLabels} currentStep={0}>
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+        const label1 = getByText(`${TEXT}-1`);
+
+        expect(label1).toHaveStyleRule('color', magma.colors.neutral700);
+        expect(label1).toHaveStyleRule(
+          'font-size',
+          magma.typographyVisualStyles.bodySmall.desktop.fontSize
+        );
+        expect(label1).toHaveStyleRule(
+          'line-height',
+          magma.typographyVisualStyles.bodySmall.desktop.lineHeight
+        );
+      });
+
+      it('should hide labels when breakpointLayout is set to "hideLabels" if viewport is smaller than set breakpoint prop', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress" 
+            breakpoint={1500}
+            breakpointLayout={StepperLayout.hideLabels}
+            currentStep={0}
+          >
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+        act(() => {
+          global.innerWidth = 1400;
+          global.dispatchEvent(new Event('resize'));
+        });
+        const label1 = getByText(`${TEXT}-1`);
+
+        expect(label1).toHaveStyleRule('height', '1px');
+        expect(label1).toHaveStyleRule('position', 'absolute');
+        expect(label1).toHaveStyleRule('overflow', 'hidden');
+        expect(label1).toHaveStyleRule('top', 'auto');
+        expect(label1).toHaveStyleRule('white-space', 'nowrap');
+        expect(label1).toHaveStyleRule('width', '1px');
+      });
+
+      it('should show labels when breakpointLayout is set to "showLabels" if viewport is smaller than set breakpoint prop', () => {
+        const { getByText } = render(
+          <Stepper ariaLabel="progress" 
+            breakpoint={1500}
+            breakpointLayout={StepperLayout.showLabels}
+            currentStep={0}
+          >
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+        act(() => {
+          global.innerWidth = 1400;
+          global.dispatchEvent(new Event('resize'));
+        });
+        const label1 = getByText(`${TEXT}-1`);
+
+        expect(label1).toHaveStyleRule('color', magma.colors.neutral700);
+        expect(label1).toHaveStyleRule(
+          'font-size',
+          magma.typographyVisualStyles.bodySmall.desktop.fontSize
+        );
+        expect(label1).toHaveStyleRule(
+          'line-height',
+          magma.typographyVisualStyles.bodySmall.desktop.lineHeight
+        );
+      });
+
+      it('should show summaryView when breakpointStyle is set to "summaryView" if viewport is smaller than set breakpoint prop', () => {
+        const { getByTestId } = render(
+          <Stepper ariaLabel="progress" 
+            testId={testId}
+            breakpoint={1500}
+            breakpointLayout={StepperLayout.summaryView}
+            currentStep={0}
+          >
+            <Step label={`${TEXT}-1`} />
+            <Step label={`${TEXT}-2`} />
+          </Stepper>
+        );
+        act(() => {
+          global.innerWidth = 1400;
+          global.dispatchEvent(new Event('resize'));
+        });
+        expect(getByTestId(`${testId}-stepper-summary`)).toBeVisible();
+        expect(getByTestId(testId)).toHaveTextContent('Step 1 of 2');
+      });
+    });
+  });
+});
