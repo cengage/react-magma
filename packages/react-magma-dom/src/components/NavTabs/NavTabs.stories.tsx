657756879777d7ae9d81b4783d627e59935cf69b
Laura Silva <silvalaurav@gmail.com>
2024-08-27 13:52:08 -0400

    fix(NavTabs): Improved accessibility by adding  for active tabs.

--- packages/react-magma-dom/src/components/NavTabs/NavTabs.stories.tsx
+++ packages/react-magma-dom/src/components/NavTabs/NavTabs.stories.tsx
@@ -4,6 +4,7 @@ import { NavTab } from './NavTab';
 import { Card } from '../Card';
 import { magma } from '../../theme/magma';
 import { Meta } from '@storybook/react/types-6-0';
+import { AndroidIcon, EmailIcon, NotificationsIcon } from 'react-magma-icons';
 
 export default {
   component: NavTabs,
@@ -17,6 +18,16 @@ export const Default = () => {
         Current Page
       </NavTab>
       <NavTab to="http://google.com">Link to Google</NavTab>
+    </NavTabs>
+  );
+};
+
+export const IconOnly = () => {
+  return (
+    <NavTabs aria-label="Icon Only Nav Tabs">
+      <NavTab aria-label="Email" icon={<EmailIcon />} to="#" isActive />
+      <NavTab aria-label="Android" icon={<AndroidIcon />} to="#" />
+      <NavTab aria-label="Notifications" icon={<NotificationsIcon />} to="#" />
     </NavTabs>
   );
 };
@@ -42,5 +53,20 @@ export const Inverse = () => {
         <NavTab to="http://apple.com">Link to Apple</NavTab>
       </NavTabs>
     </Card>
+  );
+};
+
+export const CustomTab = () => {
+  const Link = ({ to, children, ...rest }) => (
+    <a href={to} {...rest}>
+      {children}
+    </a>
+  );
+  return (
+    <NavTabs aria-label="Sample Custom Component Navigation Tabs">
+      <NavTab component={<Link to="./">Main page</Link>} />
+      <NavTab isActive component={<Link to="./">FAQ</Link>} />
+      <NavTab component={<Link to="./">About us</Link>} />
+    </NavTabs>
   );
 };
