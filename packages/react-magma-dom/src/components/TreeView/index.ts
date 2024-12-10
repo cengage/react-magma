abce2ce6df473099681650706eef3cc32d32abd5
Laura Silva <silvalaurav@gmail.com>
2024-12-10 09:04:02 -0500

    missing imports/exports

--- packages/react-magma-dom/src/components/TreeView/index.ts
+++ packages/react-magma-dom/src/components/TreeView/index.ts
@@ -4,3 +4,4 @@ export * from './useTreeItem';
 export * from './useTreeView';
 export * from './utils';
 export * from './types';
+export { TreeItemSelectedInterface, TreeViewApi } from './useTreeView';
