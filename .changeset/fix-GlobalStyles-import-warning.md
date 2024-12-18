aedb05e74e5b5f49b2bfea2880cad6963eb3630c
moathabuhamad-cengage <moath.abuhamad@cengage.com>
2024-12-18 11:52:27 +0100

    fix @import rule warning in GlobalStyles

---
+++ .changeset/fix-GlobalStyles-import-warning.md
@@ -0,0 +1,5 @@
+---
+'react-magma-dom': patch
+---
+
+fix(GlobalStyles): fix @import rule warning in GlobalStyles
--- packages/react-magma-dom/src/theme/GlobalStyles/index.tsx
+++ packages/react-magma-dom/src/theme/GlobalStyles/index.tsx
@@ -3,11 +3,14 @@ import * as React from 'react';
 import { useIsInverse } from '../../inverse';
 import { ThemeContext } from '../ThemeContext';
 
+function getGlobalImports() {
+  return css`
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,100..900;1,100..900&family=Work+Sans:ital,wght@0,100..900;1,100..900&display=swap');
+  `;
+}
+
 function getStyles(theme, isInverse: boolean) {
   return css`
-    @import url('https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Noto+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap');
-
     *,
     *:before,
     *:after {
@@ -81,7 +84,12 @@ export const GlobalStyles: React.FunctionComponent = () => {
   const isInverse = useIsInverse();
   return (
     <ThemeContext.Consumer>
-      {theme => <Global styles={getStyles(theme, isInverse)} />}
+      {theme => (
+        <>
+          <Global styles={getGlobalImports()} />
+          <Global styles={getStyles(theme, isInverse)} />
+        </>
+      )}
     </ThemeContext.Consumer>
   );
 };
