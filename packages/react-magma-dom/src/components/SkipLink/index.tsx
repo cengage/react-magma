5255a9f263d5a628356c2a8ac01af2a0ae5afb5b
Chris Cedrone <chris.cedrone@cengage.com>
2025-02-26 08:51:59 -0500

    Laura's notes

--- packages/react-magma-dom/src/components/SkipLink/index.tsx
+++ packages/react-magma-dom/src/components/SkipLink/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { ButtonColor, ButtonVariant } from '../Button';
 import { Hyperlink, HyperlinkProps } from '../Hyperlink';
+import { Omit } from '../../utils';
 import { I18nContext } from '../../i18n';
 import styled from '@emotion/styled';
 
 export const TARGET_ID = 'reactMagmaMainContent';
 
-export interface SkipLinkProps extends HyperlinkProps {
+export interface SkipLinkProps extends Omit<HyperlinkProps, 'children'> {
   /**
    * The text in the skip link
    * @default "Skip Navigation"
