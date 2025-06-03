71c0f996142c8613f3730305dad2a94199adf273
Oleh Noskov <oleh.noskov@cengage.com>
2025-06-03 10:40:12 +0300

    feat(Treeview): Support additionalContent prop. (#1736)

---
+++ .changeset/feat-TreeView-support-additionlContent-prop.md
@@ -0,0 +1,5 @@
+---
+'react-magma-dom': minor
+---
+
+Feat(TreeView): Add `additionalContent` prop. Removed animation from Checkbox and Radio components.
--- packages/react-magma-dom/src/utils/index.ts
+++ packages/react-magma-dom/src/utils/index.ts
@@ -333,3 +333,15 @@ export function collectTextFromReactNode(node: React.ReactNode) {
 
   return result.join(' ');
 }
+
+export const mergeRefs = <T>(...refs: Array<React.Ref<T> | undefined>) => {
+  return (node: T | null) => {
+    refs.forEach(ref => {
+      if (typeof ref === 'function') {
+        ref(node);
+      } else if (ref !== null) {
+        (ref as React.MutableRefObject<T | null>).current = node;
+      }
+    });
+  };
+};
