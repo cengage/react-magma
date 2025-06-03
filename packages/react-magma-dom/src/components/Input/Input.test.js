5263726555865446fc5339938c623392ff442318
Oleh Noskov <oleh.noskov@cengage.com>
2025-06-03 11:11:37 +0300

    fix(Input): Does not update value as expected (#1782)
    Co-authored-by: moathabuhamad-cengage <moath.abuhamad@cengage.com>

--- packages/react-magma-dom/src/components/Input/Input.test.js
+++ packages/react-magma-dom/src/components/Input/Input.test.js
@@ -1,15 +1,28 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { transparentize } from 'polished';
+import PropTypes from 'prop-types';
 import { CheckIcon } from 'react-magma-icons';
 
 import { axe } from '../../../axe-helper';
 import { defaultI18n } from '../../i18n/default';
 import { magma } from '../../theme/magma';
 import { InputType } from '../InputBase';
+import { CustomTopicsRow } from './testUtils';
 
 import { Input } from '.';
+
+const renderWithProviders = (ui, renderOptions = {}) => {
+  const Wrapper = ({ children }) => <>{children}</>;
+
+  Wrapper.propTypes = {
+    children: PropTypes.node,
+  };
+
+  return render(ui, { wrapper: Wrapper, ...renderOptions });
+};
 
 const label = 'test label';
 
@@ -704,6 +717,73 @@ describe('Input', () => {
 
       const inputElement = getByLabelText('Url');
       expect(inputElement).toHaveAttribute('type', 'url');
+    });
+  });
+
+  describe('Custom Topics Row', () => {
+    const properties = {
+      topicList: [
+        {
+          reference: 'custom sub activity reference',
+          title: 'custom sub activity title',
+        },
+      ],
+      isRemoveButtonDisabled: false,
+      shouldValidate: true,
+      topicTitle: '',
+      testTopic: undefined,
+      studyMaterialsTopic: undefined,
+      order: 1,
+      setTopicTitle: jest.fn(),
+      setTestTopic: jest.fn(),
+      setStudyMaterialsTopic: jest.fn(),
+      removeTopicRow: jest.fn(),
+    };
+
+    it('should insert title topic', async () => {
+      renderWithProviders(<CustomTopicsRow {...properties} />);
+
+      await userEvent.type(screen.getByTestId('topicTitle-1'), 'topicTitle');
+
+      expect(properties.setTopicTitle).toHaveBeenCalledWith('topicTitle');
+    });
+
+    it('should select test topic', async () => {
+      renderWithProviders(<CustomTopicsRow {...properties} />);
+
+      await userEvent.click(
+        screen.getByRole('combobox', { name: 'Topic 1 Test *' })
+      );
+      await userEvent.click(
+        screen.getByRole('option', { name: 'custom sub activity title' })
+      );
+
+      expect(properties.setTestTopic).toHaveBeenCalledWith(
+        'custom sub activity reference'
+      );
+    });
+
+    it('should select study materials topic', async () => {
+      renderWithProviders(<CustomTopicsRow {...properties} />);
+
+      await userEvent.click(
+        screen.getByRole('combobox', { name: 'Topic 1 Study Materials *' })
+      );
+      await userEvent.click(
+        screen.getByRole('option', { name: 'custom sub activity title' })
+      );
+
+      expect(properties.setStudyMaterialsTopic).toHaveBeenCalledWith(
+        'custom sub activity reference'
+      );
+    });
+
+    it('should remove custom topics row', async () => {
+      renderWithProviders(<CustomTopicsRow {...properties} />);
+
+      await userEvent.click(screen.getByTestId('removeRowButton-1'));
+
+      expect(properties.removeTopicRow).toHaveBeenCalled();
     });
   });
 });
