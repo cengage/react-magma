e82a0d71263367073c95a3b3d74f22f8a3d64e07
Oleh Noskov <oleh.noskov@cengage.com>
2025-06-06 14:28:33 +0300

    feat(drawer): add should show background overlay prop

--- packages/react-magma-dom/src/components/Drawer/Drawer.test.js
+++ packages/react-magma-dom/src/components/Drawer/Drawer.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 
 import { axe } from '../../../axe-helper';
+import { magma } from '../../theme/magma';
 
 import { Drawer } from '.';
 
@@ -98,5 +99,49 @@ describe('Drawer', () => {
     const results = await axe(baseElement);
 
     return expect(results).toHaveNoViolations();
+  });
+
+  describe('showBackgroundOverlay prop', () => {
+    const drawerContent = 'Drawer content';
+    const modalBackDropTestId = 'modal-backdrop';
+    const modalContentTestId = 'modal-content';
+
+    it('should show background overlay when showBackgroundOverlay is true', async () => {
+      const { getByTestId } = render(
+        <Drawer position="bottom" header="Hello" isOpen testId={TEST_ID}>
+          {drawerContent}
+        </Drawer>
+      );
+
+      const modalContent = getByTestId(modalContentTestId);
+
+      expect(modalContent).toBeInTheDocument();
+      expect(getByTestId(modalBackDropTestId)).toBeInTheDocument();
+      expect(modalContent).toHaveStyle(`border: none`);
+    });
+
+    it('should hide background overlay when showBackgroundOverlay is false', async () => {
+      const { queryByTestId } = render(
+        <Drawer
+          position="bottom"
+          header="Hello"
+          isOpen
+          testId={TEST_ID}
+          showBackgroundOverlay={false}
+          isInverse
+        >
+          {drawerContent}
+        </Drawer>
+      );
+
+      const modalContent = queryByTestId(modalContentTestId);
+
+      expect(modalContent).toBeInTheDocument();
+      expect(queryByTestId(modalBackDropTestId)).not.toBeInTheDocument();
+      expect(modalContent).toHaveStyleRule(
+        `border`,
+        `1px solid ${magma.colors.primary400}`
+      );
+    });
   });
 });
