c80ab602ab3f518cad153ce6ea0a078d5675d8bd
Oleh Noskov <oleh.noskov@cengage.com>
2025-07-01 18:19:50 +0300

    chore: fixing all mdx/implementation files

--- packages/schema-renderer/src/components/BasicTemplate/index.tsx
+++ packages/schema-renderer/src/components/BasicTemplate/index.tsx
@@ -5,9 +5,11 @@ import { FormTemplateRenderProps } from '@data-driven-forms/react-form-renderer'
 export const BasicTemplate = ({ formFields }: FormTemplateRenderProps) => {
   return (
     <div>
-      {formFields.map((Field, index) => (
-        <Field key={index} />
-      ))}
+      {formFields.map((field, index) =>
+        React.isValidElement(field)
+          ? React.cloneElement(field, { key: index })
+          : null
+      )}
     </div>
   );
 };
