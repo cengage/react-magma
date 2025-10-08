d892a6199ca4023a96d50ab938d136b82d4243c0
Oleh Noskov <oleh.noskov@cengage.com>
2025-10-08 16:50:06 +0300

    chore: fix announce, button docs

--- packages/react-magma-dom/src/components/AppBar/AppBar.stories.tsx
+++ packages/react-magma-dom/src/components/AppBar/AppBar.stories.tsx
@@ -102,14 +102,17 @@ export const WithSkipLink = () => {
         tribal knowledge increase the pipelines, but run it up the flag pole.
       </Paragraph>
       <SkipLinkContent>
-        <Paragraph id="last-one">
-          Turn the crank regroup can we align on lunch orders, so regroup, yet
-          the last person we talked to said this would be ready, for this vendor
-          is incompetent , or pipeline. We&apos;ve bootstrapped the model
-          zeitgeist, so let&apos;s schedule a standup during the sprint to
-          review our kpis, so not the long pole in my tent we&apos;re ahead of
-          the curve on that one, and we&apos;re ahead of the curve on that one.
-        </Paragraph>
+        <div style={{ backgroundColor: magma.colors.neutral100 }}>
+          <Paragraph id="last-one" style={{ color: magma.colors.neutral700 }}>
+            Turn the crank regroup can we align on lunch orders, so regroup, yet
+            the last person we talked to said this would be ready, for this
+            vendor is incompetent , or pipeline. We&apos;ve bootstrapped the
+            model zeitgeist, so let&apos;s schedule a standup during the sprint
+            to review our kpis, so not the long pole in my tent we&apos;re ahead
+            of the curve on that one, and we&apos;re ahead of the curve on that
+            one.
+          </Paragraph>
+        </div>
       </SkipLinkContent>
     </div>
   );
