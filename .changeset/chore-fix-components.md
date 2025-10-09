4fea6ca764a6c0bbc23751b0e7086df713c36f4c
Oleh Noskov <oleh.noskov@cengage.com>
2025-10-09 12:37:20 +0300

    chore: fix export props issue for storybook

--- .changeset/chore-fix-components.md
+++ .changeset/chore-fix-components.md
@@ -2,4 +2,4 @@
 'react-magma-docs': patch
 ---
 
-chore: fix storybook issue (Combobox) and docs examples ().
+chore: fix storybook issue (Combobox) and terminal export errors and docs examples (Button).
--- packages/react-magma-dom/src/index.ts
+++ packages/react-magma-dom/src/index.ts
@@ -7,23 +7,24 @@ export {
   AIButtonTextTransform,
   AIButtonType,
   AIButtonVariant,
+} from './components/AIButton';
+export type {
   IconOnlyAIButtonProps,
   TextAIButtonProps,
 } from './components/AIButton';
-export { Alert, AlertProps } from './components/Alert';
+export { Alert } from './components/Alert';
+export type { AlertProps } from './components/Alert';
 export { AlertVariant } from './components/AlertBase';
-export { Announce, AnnounceProps } from './components/Announce';
-export { AppBar, AppBarProps, AppBarPosition } from './components/AppBar';
-export {
-  Badge,
-  BadgeColor,
-  BadgeProps,
-  BadgeVariant,
-} from './components/Badge';
-export { Banner, BannerProps } from './components/Banner';
-export {
-  Breadcrumb,
-  BreadcrumbItem,
+export { Announce } from './components/Announce';
+export type { AnnounceProps } from './components/Announce';
+export { AppBar, AppBarPosition } from './components/AppBar';
+export type { AppBarProps } from './components/AppBar';
+export { Badge, BadgeColor, BadgeVariant } from './components/Badge';
+export type { BadgeProps } from './components/Badge';
+export { Banner } from './components/Banner';
+export type { BannerProps } from './components/Banner';
+export { Breadcrumb, BreadcrumbItem } from './components/Breadcrumb';
+export type {
   BreadcrumbItemProps,
   BreadcrumbProps,
 } from './components/Breadcrumb';
@@ -31,6 +32,8 @@ export {
   BreakpointsContainer,
   Breakpoint,
   BreakpointScreenSize,
+} from './components/BreakpointsContainer';
+export type {
   BreakpointProps,
   BreakpointsContainerProps,
 } from './components/BreakpointsContainer';
@@ -42,124 +45,94 @@ export {
   ButtonVariant,
   ButtonTextTransform,
   ButtonType,
-  ButtonProps,
 } from './components/Button';
-export {
-  Card,
-  CardAlignment,
-  CardCalloutType,
-  CardProps,
-} from './components/Card';
+export type { ButtonProps } from './components/Button';
+export { Card, CardAlignment, CardCalloutType } from './components/Card';
+export type { CardProps } from './components/Card';
 export { CardBody } from './components/Card/CardBody';
 export { CardHeading } from './components/Card/CardHeading';
-export {
-  Checkbox,
-  CheckboxTextPosition,
-  CheckboxProps,
-} from './components/Checkbox';
-export { Container, ContainerProps } from './components/Container';
-export { Datagrid, DatagridProps } from './components/Datagrid';
-export { DatePicker, DatePickerProps } from './components/DatePicker';
-export {
-  DateTimePicker,
-  DateTimePickerProps,
-} from './components/DateTimePicker';
+export { Checkbox, CheckboxTextPosition } from './components/Checkbox';
+export type { CheckboxProps } from './components/Checkbox';
+export { Container } from './components/Container';
+export type { ContainerProps } from './components/Container';
+export { Datagrid } from './components/Datagrid';
+export type { DatagridProps } from './components/Datagrid';
+export { DatePicker } from './components/DatePicker';
+export type { DatePickerProps } from './components/DatePicker';
+export { DateTimePicker } from './components/DateTimePicker';
+export type { DateTimePickerProps } from './components/DateTimePicker';
 export { inDateRange, getDateFromString } from './components/DatePicker/utils';
 export {
   Dropdown,
   DropdownAlignment,
   DropdownDropDirection,
-  DropdownProps,
 } from './components/Dropdown';
-export {
-  DropdownContent,
-  DropdownContentProps,
-} from './components/Dropdown/DropdownContent';
-export {
-  DropdownDivider,
-  DropdownDividerProps,
-} from './components/Dropdown/DropdownDivider';
-export {
-  DropdownHeader,
-  DropdownHeaderProps,
-} from './components/Dropdown/DropdownHeader';
-export {
-  DropdownMenuGroup,
-  DropdownMenuGroupProps,
-} from './components/Dropdown/DropdownMenuGroup';
-export {
-  DropdownMenuItem,
-  DropdownMenuItemProps,
-} from './components/Dropdown/DropdownMenuItem';
-export {
-  DropdownMenuNavItem,
-  DropdownMenuNavItemProps,
-} from './components/Dropdown/DropdownMenuNavItem';
-export {
-  DropdownSplitButton,
-  DropdownSplitButtonProps,
-} from './components/Dropdown/DropdownSplitButton';
-export {
-  DropdownButton,
-  DropdownButtonProps,
-} from './components/Dropdown/DropdownButton';
+export type { DropdownProps } from './components/Dropdown';
+export { DropdownContent } from './components/Dropdown/DropdownContent';
+export type { DropdownContentProps } from './components/Dropdown/DropdownContent';
+export { DropdownDivider } from './components/Dropdown/DropdownDivider';
+export type { DropdownDividerProps } from './components/Dropdown/DropdownDivider';
+export { DropdownHeader } from './components/Dropdown/DropdownHeader';
+export type { DropdownHeaderProps } from './components/Dropdown/DropdownHeader';
+export { DropdownMenuGroup } from './components/Dropdown/DropdownMenuGroup';
+export type { DropdownMenuGroupProps } from './components/Dropdown/DropdownMenuGroup';
+export { DropdownMenuItem } from './components/Dropdown/DropdownMenuItem';
+export type { DropdownMenuItemProps } from './components/Dropdown/DropdownMenuItem';
+export { DropdownMenuNavItem } from './components/Dropdown/DropdownMenuNavItem';
+export type { DropdownMenuNavItemProps } from './components/Dropdown/DropdownMenuNavItem';
+export { DropdownSplitButton } from './components/Dropdown/DropdownSplitButton';
+export type { DropdownSplitButtonProps } from './components/Dropdown/DropdownSplitButton';
+export { DropdownButton } from './components/Dropdown/DropdownButton';
+export type { DropdownButtonProps } from './components/Dropdown/DropdownButton';
 export { DropdownExpandableMenuGroup } from './components/Dropdown/DropdownExpandableMenuGroup';
-export {
-  DropdownExpandableMenuItem,
-  DropdownExpandableMenuItemProps,
-} from './components/Dropdown/DropdownExpandableMenuItem';
-export {
-  DropdownExpandableMenuListItem,
-  DropdownExpandableMenuListItemProps,
-} from './components/Dropdown/DropdownExpandableMenuListItem';
-export {
-  DropdownExpandableMenuButton,
-  DropdownExpandableMenuButtonProps,
-} from './components/Dropdown/DropdownExpandableMenuButton';
-export {
-  DropdownExpandableMenuPanel,
-  DropdownExpandableMenuPanelProps,
-} from './components/Dropdown/DropdownExpandableMenuPanel';
+export { DropdownExpandableMenuItem } from './components/Dropdown/DropdownExpandableMenuItem';
+export type { DropdownExpandableMenuItemProps } from './components/Dropdown/DropdownExpandableMenuItem';
+export { DropdownExpandableMenuListItem } from './components/Dropdown/DropdownExpandableMenuListItem';
+export type { DropdownExpandableMenuListItemProps } from './components/Dropdown/DropdownExpandableMenuListItem';
+export { DropdownExpandableMenuButton } from './components/Dropdown/DropdownExpandableMenuButton';
+export type { DropdownExpandableMenuButtonProps } from './components/Dropdown/DropdownExpandableMenuButton';
+export { DropdownExpandableMenuPanel } from './components/Dropdown/DropdownExpandableMenuPanel';
+export type { DropdownExpandableMenuPanelProps } from './components/Dropdown/DropdownExpandableMenuPanel';
+
 export * from './components/Flex';
-export { Form, FormProps } from './components/Form';
-export { FormGroup, FormGroupProps } from './components/FormGroup';
-export { Heading, HeadingProps } from './components/Heading';
+export { Form } from './components/Form';
+export type { FormProps } from './components/Form';
+export { FormGroup } from './components/FormGroup';
+export type { FormGroupProps } from './components/FormGroup';
+export { Heading } from './components/Heading';
+export type { HeadingProps } from './components/Heading';
 export {
   HideAtBreakpoint,
   HideAtBreakpointDisplayType,
-  HideAtBreakpointProps,
 } from './components/HideAtBreakpoint';
-export {
-  Hyperlink,
-  HyperlinkIconPosition,
-  HyperlinkProps,
-} from './components/Hyperlink';
-export {
-  IconButton,
-  ButtonIconPosition,
-  IconButtonProps,
-} from './components/IconButton';
+export type { HideAtBreakpointProps } from './components/HideAtBreakpoint';
+export { Hyperlink, HyperlinkIconPosition } from './components/Hyperlink';
+export type { HyperlinkProps } from './components/Hyperlink';
+export { IconButton, ButtonIconPosition } from './components/IconButton';
+export type { IconButtonProps } from './components/IconButton';
 export {
   IndeterminateCheckbox,
-  IndeterminateCheckboxProps,
   IndeterminateCheckboxStatus,
 } from './components/IndeterminateCheckbox';
-export { Input, InputProps } from './components/Input';
+export type { IndeterminateCheckboxProps } from './components/IndeterminateCheckbox';
+export { Input } from './components/Input';
+export type { InputProps } from './components/Input';
 export {
   InputBase,
-  InputBaseProps,
   InputIconPosition,
   InputSize,
   InputType,
 } from './components/InputBase';
+export type { InputBaseProps } from './components/InputBase';
 export { InputMessage } from './components/Input/InputMessage';
 export { Label, LabelPosition } from './components/Label';
 export {
   LoadingIndicator,
-  LoadingIndicatorProps,
   LoadingIndicatorType,
 } from './components/LoadingIndicator';
-export { NativeSelect, NativeSelectProps } from './components/NativeSelect';
+export type { LoadingIndicatorProps } from './components/LoadingIndicator';
+export { NativeSelect } from './components/NativeSelect';
+export type { NativeSelectProps } from './components/NativeSelect';
 export { NavTabs } from './components/NavTabs';
 export { NavTab } from './components/NavTabs/NavTab';
 export {
@@ -167,18 +140,22 @@ export {
   PageButtonSize,
   PaginationType,
 } from './components/Pagination';
-export { Paragraph, ParagraphProps } from './components/Paragraph';
-export { PasswordInput, PasswordInputProps } from './components/PasswordInput';
-export {
-  ProgressBar,
-  ProgressBarColor,
-  ProgressBarProps,
-} from './components/ProgressBar';
-export { ProgressRing, ProgressRingProps } from './components/ProgressRing';
-export { RadioGroup, RadioGroupProps } from './components/RadioGroup';
-export { Radio, RadioTextPosition, RadioProps } from './components/Radio';
-export { Modal, ModalSize, ModalProps } from './components/Modal';
-export { Search, SearchProps } from './components/Search';
+export { Paragraph } from './components/Paragraph';
+export type { ParagraphProps } from './components/Paragraph';
+export { PasswordInput } from './components/PasswordInput';
+export type { PasswordInputProps } from './components/PasswordInput';
+export { ProgressBar, ProgressBarColor } from './components/ProgressBar';
+export type { ProgressBarProps } from './components/ProgressBar';
+export { ProgressRing } from './components/ProgressRing';
+export type { ProgressRingProps } from './components/ProgressRing';
+export { RadioGroup } from './components/RadioGroup';
+export type { RadioGroupProps } from './components/RadioGroup';
+export { Radio, RadioTextPosition } from './components/Radio';
+export type { RadioProps } from './components/Radio';
+export { Modal, ModalSize } from './components/Modal';
+export type { ModalProps } from './components/Modal';
+export { Search } from './components/Search';
+export type { SearchProps } from './components/Search';
 
 export {
   Select,
@@ -187,49 +164,54 @@ export {
 } from './components/Select';
 export { Combobox, ComboboxStateChangeTypes } from './components/Combobox';
 
-export { SkipLink, SkipLinkProps } from './components/SkipLink';
-export {
-  SkipLinkContent,
-  SkipLinkContentProps,
-} from './components/SkipLinkContent';
-export { Spacer, SpacerProps, SpacerAxis } from './components/Spacer';
-export { Spinner, SpinnerProps } from './components/Spinner';
+export { SkipLink } from './components/SkipLink';
+export type { SkipLinkProps } from './components/SkipLink';
+export { SkipLinkContent } from './components/SkipLinkContent';
+export type { SkipLinkContentProps } from './components/SkipLinkContent';
+export { Spacer, SpacerAxis } from './components/Spacer';
+export type { SpacerProps } from './components/Spacer';
+export { Spinner } from './components/Spinner';
+export type { SpinnerProps } from './components/Spinner';
 export {
   Stepper,
   StepperLayout,
-  StepperProps,
   StepperOrientation,
+} from './components/Stepper';
+export type {
+  StepperProps,
   ResponsiveStepperContainerProps,
 } from './components/Stepper';
-export { Step, StepProps } from './components/Stepper/Step';
+export { Step } from './components/Stepper/Step';
+export type { StepProps } from './components/Stepper/Step';
 export { ResponsiveStepperContainer } from './components/Stepper';
 export {
   Table,
-  TableProps,
   TableDensity,
   TableCellAlign,
   TableContext,
   TableSortDirection,
   TableBody,
-  TableBodyProps,
   TableCell,
-  TableCellProps,
   TableHead,
-  TableHeadProps,
   TableHeaderCell,
-  TableHeaderCellProps,
   TableHeaderCellScope,
   TablePagination,
-  TablePaginationProps,
   TableRow,
   TableRowColor,
+} from './components/Table';
+
+export type {
+  TableProps,
+  TableBodyProps,
+  TableCellProps,
+  TableHeadProps,
+  TableHeaderCellProps,
+  TablePaginationProps,
   TableRowProps,
 } from './components/Table';
 
-export {
-  TabsContainer,
-  TabsContainerProps,
-} from './components/Tabs/TabsContainer';
+export { TabsContainer } from './components/Tabs/TabsContainer';
+export type { TabsContainerProps } from './components/Tabs/TabsContainer';
 export {
   Tabs,
   TabsAlignment,
@@ -239,81 +221,74 @@ export {
 export { TabsOrientation, TabsTextTransform } from './components/Tabs/shared';
 export { Tab } from './components/Tabs/Tab';
 export { CustomTab } from './components/Tabs/CustomTab';
-export {
-  TabPanelsContainer,
-  TabPanelContainerProps,
-} from './components/Tabs/TabPanelsContainer';
+export { TabPanelsContainer } from './components/Tabs/TabPanelsContainer';
+export type { TabPanelContainerProps } from './components/Tabs/TabPanelsContainer';
 export { TabPanel } from './components/Tabs/TabPanel';
 export { TabsScrollSpyContainer } from './components/Tabs/TabsScrollSpyContainer';
 export { TabScrollSpyPanel } from './components/Tabs/TabScrollSpyPanel';
 
-export { Tag, TagProps } from './components/Tag';
+export { Tag } from './components/Tag';
+export type { TagProps } from './components/Tag';
 
-export {
-  ToastsContainer,
-  ToastsContainerProps,
-} from './components/Toast/ToastsContainer';
-export { Toast, ToastProps } from './components/Toast';
-export { Toggle, ToggleTextPosition, ToggleProps } from './components/Toggle';
+export { ToastsContainer } from './components/Toast/ToastsContainer';
+export type { ToastsContainerProps } from './components/Toast/ToastsContainer';
+export { Toast } from './components/Toast';
+export type { ToastProps } from './components/Toast';
+export { Toggle, ToggleTextPosition } from './components/Toggle';
+export type { ToggleProps } from './components/Toggle';
 export {
   EnumTooltipPosition,
   StyledTooltip,
   Tooltip,
   TooltipArrow,
   TooltipPosition,
-  TooltipProps,
 } from './components/Tooltip';
-export { Textarea, TextareaProps } from './components/Textarea';
-export { TimePicker, TimePickerProps } from './components/TimePicker';
+export type { TooltipProps } from './components/Tooltip';
+export { Textarea } from './components/Textarea';
+export type { TextareaProps } from './components/Textarea';
+export { TimePicker } from './components/TimePicker';
+export type { TimePickerProps } from './components/TimePicker';
 export {
-  TypographyProps,
   TypographyColor,
   TypographyContextVariant,
   TypographyVisualStyle,
 } from './components/Typography';
+export type { TypographyProps } from './components/Typography';
 export {
   Popover,
-  PopoverProps,
   PopoverPosition,
   PopoverAlignment,
-  PopoverApi,
 } from './components/Popover';
-export {
-  PopoverTrigger,
-  PopoverTriggerProps,
-} from './components/Popover/PopoverTrigger';
-export {
-  PopoverContent,
-  PopoverContentProps,
-} from './components/Popover/PopoverContent';
+export type { PopoverProps, PopoverApi } from './components/Popover';
+export { PopoverTrigger } from './components/Popover/PopoverTrigger';
+export type { PopoverTriggerProps } from './components/Popover/PopoverTrigger';
+export { PopoverContent } from './components/Popover/PopoverContent';
+export type { PopoverContentProps } from './components/Popover/PopoverContent';
 export {
   PopoverHeader,
   PopoverFooter,
-  PopoverSectionProps,
 } from './components/Popover/PopoverSection';
+export type { PopoverSectionProps } from './components/Popover/PopoverSection';
 
-export {
-  VisuallyHidden,
-  VisuallyHiddenProps,
-} from './components/VisuallyHidden';
+export { VisuallyHidden } from './components/VisuallyHidden';
+export type { VisuallyHiddenProps } from './components/VisuallyHidden';
 
 export {
   DefinitionListItem,
   DefinitionListType,
-  DefinitionListItemProps,
 } from './components/DefinitionList/DefinitionListItem';
-export {
-  DefinitionList,
-  DefinitionListProps,
-} from './components/DefinitionList/DefinitionList';
+export type { DefinitionListItemProps } from './components/DefinitionList/DefinitionListItem';
+export { DefinitionList } from './components/DefinitionList/DefinitionList';
+export type { DefinitionListProps } from './components/DefinitionList/DefinitionList';
 
-export { magma, ThemeInterface } from './theme/magma';
+export { magma } from './theme/magma';
+export type { ThemeInterface } from './theme/magma';
 export { GlobalStyles } from './theme/GlobalStyles';
 export { ThemeContext } from './theme/ThemeContext';
 export { default as styled } from '@emotion/styled';
 export { I18nContext } from './i18n';
 export { defaultI18n } from './i18n/default';
-export { I18nInterface } from './i18n/interface';
+export type { I18nInterface } from './i18n/interface';
 export { useMediaQuery } from './hooks/useMediaQuery';
 export { useDeviceDetect } from './hooks/useDeviceDetect';
 export { useDataPagination } from './hooks/useDataPagination';
