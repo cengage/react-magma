2e6c1e4417e9d32b84aff4025090583f9313e44b
nikitaorliak-cengage <nikita.orliak@cengage.com>
2025-11-18 16:26:12 +0100

    feat(datetimepicker): add timezone (#2026)
    Co-authored-by: Nikita Orliak <nikita.orliak@nixs.com>

--- packages/react-magma-dom/src/components/TimePicker/TimePicker.tsx
+++ packages/react-magma-dom/src/components/TimePicker/TimePicker.tsx
@@ -134,6 +134,12 @@ const StyledNumInput = styled.input<{
   }
 `;
 
+const InputsWithTimezone = styled.div`
+  display: flex;
+  align-items: center;
+  gap: ${props => props.theme.spaceScale.spacing03};
+`;
+
 export const TimePicker = React.forwardRef<HTMLInputElement, TimePickerProps>(
   (props, ref) => {
     const theme = React.useContext(ThemeContext);
@@ -149,6 +155,7 @@ export const TimePicker = React.forwardRef<HTMLInputElement, TimePickerProps>(
       labelText,
       labelWidth,
       minutesStep,
+      timezone,
       onChange,
       ...other
     } = props;
@@ -198,91 +205,94 @@ export const TimePicker = React.forwardRef<HTMLInputElement, TimePickerProps>(
         labelPosition={labelPosition}
         labelWidth={labelWidth}
       >
-        <InputsContainer
-          isInverse={isInverse}
-          hasError={!!errorMessage}
-          theme={theme}
-          style={inputStyle}
-        >
-          <ScheduleIcon
-            color={
-              isInverse ? theme.colors.neutral100 : theme.colors.neutral700
-            }
-            style={{ marginRight: theme.spaceScale.spacing02 }}
-          />
-          <StyledNumInput
-            aria-label={hoursLabel}
-            aria-describedby={descriptionId}
-            data-testid="hoursTimeInput"
-            id={hourId}
+        <InputsWithTimezone theme={theme}>
+          <InputsContainer
             isInverse={isInverse}
-            maxLength={2}
-            max="12"
-            min="1"
-            isFocused={hasTime || isFocused}
-            onChange={handleHourChange}
-            onKeyDown={e => handleHourKeyDown(e, handleHourChange)}
-            placeholder="--"
-            ref={hourRef}
+            hasError={!!errorMessage}
             theme={theme}
-            type="number"
-            value={hour}
-            onFocus={e => {
-              e.target.select();
-              setIsFocused(true);
-            }}
-            onBlur={() => setIsFocused(false)}
-          />
-          <Divider
-            isInverse={isInverse}
-            isFocused={hasTime || isFocused}
-            theme={theme}
+            style={inputStyle}
           >
-            :
-          </Divider>
-          <StyledNumInput
-            aria-label={minutesLabel}
-            data-testid="minutesTimeInput"
-            id={minuteId}
-            isInverse={isInverse}
-            maxLength={2}
-            max="59"
-            min="0"
-            isFocused={hasTime || isFocused}
-            onChange={handleMinuteChange}
-            onKeyDown={e => handleMinuteKeyDown(e, handleMinuteChange)}
-            placeholder="--"
-            ref={minuteRef}
-            step={minutesStep || 1}
-            theme={theme}
-            type="number"
-            value={minute}
-            onFocus={e => {
-              e.target.select();
-              setIsFocused(true);
-            }}
-            onBlur={() => setIsFocused(false)}
-          />
-          <AmPmToggle
-            aria-label={amPmLabel}
-            isInverse={isInverse}
-            ref={amPmRef}
-            onClick={toggleAmPm}
-            onKeyDown={handleAmPmKeyDown}
-            onFocus={() => setIsFocused(true)}
-            onBlur={() => setIsFocused(false)}
-            isFocused={hasTime || isFocused}
-          >
-            {amPm}
-          </AmPmToggle>
-          <VisuallyHidden>
-            <Announce>
-              {amPm === am
-                ? i18n.timePicker.amSelectedAnnounce
-                : i18n.timePicker.pmSelectedAnnounce}
-            </Announce>
-          </VisuallyHidden>
-        </InputsContainer>
+            <ScheduleIcon
+              color={
+                isInverse ? theme.colors.neutral100 : theme.colors.neutral700
+              }
+              style={{ marginRight: theme.spaceScale.spacing02 }}
+            />
+            <StyledNumInput
+              aria-label={hoursLabel}
+              aria-describedby={descriptionId}
+              data-testid="hoursTimeInput"
+              id={hourId}
+              isInverse={isInverse}
+              maxLength={2}
+              max="12"
+              min="1"
+              isFocused={hasTime || isFocused}
+              onChange={handleHourChange}
+              onKeyDown={e => handleHourKeyDown(e, handleHourChange)}
+              placeholder="--"
+              ref={hourRef}
+              theme={theme}
+              type="number"
+              value={hour}
+              onFocus={e => {
+                e.target.select();
+                setIsFocused(true);
+              }}
+              onBlur={() => setIsFocused(false)}
+            />
+            <Divider
+              isInverse={isInverse}
+              isFocused={hasTime || isFocused}
+              theme={theme}
+            >
+              :
+            </Divider>
+            <StyledNumInput
+              aria-label={minutesLabel}
+              data-testid="minutesTimeInput"
+              id={minuteId}
+              isInverse={isInverse}
+              maxLength={2}
+              max="59"
+              min="0"
+              isFocused={hasTime || isFocused}
+              onChange={handleMinuteChange}
+              onKeyDown={e => handleMinuteKeyDown(e, handleMinuteChange)}
+              placeholder="--"
+              ref={minuteRef}
+              step={minutesStep || 1}
+              theme={theme}
+              type="number"
+              value={minute}
+              onFocus={e => {
+                e.target.select();
+                setIsFocused(true);
+              }}
+              onBlur={() => setIsFocused(false)}
+            />
+            <AmPmToggle
+              aria-label={amPmLabel}
+              isInverse={isInverse}
+              ref={amPmRef}
+              onClick={toggleAmPm}
+              onKeyDown={handleAmPmKeyDown}
+              onFocus={() => setIsFocused(true)}
+              onBlur={() => setIsFocused(false)}
+              isFocused={hasTime || isFocused}
+            >
+              {amPm}
+            </AmPmToggle>
+            <VisuallyHidden>
+              <Announce>
+                {amPm === am
+                  ? i18n.timePicker.amSelectedAnnounce
+                  : i18n.timePicker.pmSelectedAnnounce}
+              </Announce>
+            </VisuallyHidden>
+          </InputsContainer>
+          {timezone}
+        </InputsWithTimezone>
         <input id={id} ref={ref} type="hidden" value={time} />
       </FormFieldContainer>
     );
