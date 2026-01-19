f7ec34cfc1463d9eb13648de8ebe79314d18a4db
Oleh Noskov <oleh.noskov@cengage.com>
2026-01-19 12:26:12 +0200

    fix(Dropdown): Add role=menu to DropdownContent for DropdownMenuNavItem children. (#2190)

---
+++ .changeset/fix-Dropdown-accessibility-issues.md
@@ -0,0 +1,5 @@
+---
+'react-magma-dom': patch
+---
+
+fix(Dropdown): Remove `aria-label` from DropdownMenuItem icon. Add `role=menu` to DropdownContent for DropdownMenuNavItem children. Fix docs examples.
--- packages/react-magma-dom/src/components/Dropdown/DropdownContent.tsx
+++ packages/react-magma-dom/src/components/Dropdown/DropdownContent.tsx
@@ -79,7 +79,8 @@ export const DropdownContent = React.forwardRef<
   React.Children.forEach(children, (child: any) => {
     if (
       child?.type?.displayName === 'DropdownMenuItem' ||
-      child?.type?.displayName === 'DropdownMenuGroup'
+      child?.type?.displayName === 'DropdownMenuGroup' ||
+      child?.type?.displayName === 'DropdownMenuNavItem'
     ) {
       hasItemChildren = true;
     }
