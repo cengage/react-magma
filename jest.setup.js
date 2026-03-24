0a2188e72dc0051d305aca461756d8f75faafcd9
moathabuhamad-cengage <moath.abuhamad@cengage.com>
2026-03-24 07:53:27 +0100

    chore(build): fix jest console warnings

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,10 @@
 global.IS_REACT_ACT_ENVIRONMENT = true;
 
+HTMLCanvasElement.prototype.getContext = jest.fn(() => ({
+  font: '',
+  measureText: jest.fn(() => ({ width: 0 })),
+}));
+
 const { createSerializer, matchers } = require('@emotion/jest');
 const testingLibraryJestDom = require('@testing-library/jest-dom');
 const reactTestingLibrary = require('@testing-library/react');
