d27221ff52957cf5c7abae7fb871077a6b8e1d57
Oleh Noskov <oleh.noskov@cengage.com>
2026-04-02 12:02:57 +0300

    fix(Datagrid & Table): Update the table header aria-label. (#2399)

--- packages/react-magma-dom/src/components/Table/TableHeaderCell.tsx
+++ packages/react-magma-dom/src/components/Table/TableHeaderCell.tsx
@@ -11,7 +11,7 @@ import {
   TableSortDirection,
 } from './Table';
 import { baseTableCellStyle, buildCellPaddingStyle } from './TableCell';
-import { getAriaSort, getTableSortIcon } from './utils';
+import { getAriaSort, getAriaSortLabel, getTableSortIcon } from './utils';
 import { I18nContext } from '../..';
 import { ThemeContext } from '../../theme/ThemeContext';
 
@@ -207,8 +207,11 @@ export const TableHeaderCell = React.forwardRef<
 
   const widthString = typeof width === 'number' ? `${width}px` : width;
 
+  const sortDirectionLabel =
+    getAriaSortLabel(sortDirection) || i18n.table.selectable.sortDirectionNone;
   const sortRowsAriaLabel = isSortable
-    ? i18n.table.selectable.sortButtonAriaLabel.replace('{labelText}', header)
+    ? i18n.table.selectable.sortButtonAriaLabel.replace('{labelText}', header) +
+      `, ${sortDirectionLabel}`
     : undefined;
 
   return (
