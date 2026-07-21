661cf8d61e32d1bbdf6288c1b4383be00a4e6a94
Serhii Yanhol <serhii.yangol@gmail.com>
2026-07-21 15:09:24 +0300

    fix(Accordion): stop overriding Heading styles inside AccordionPanel (#2600)
    Co-authored-by: Serhii Yanhol <serhii@airwayz,co>

--- packages/react-magma-dom/src/components/Accordion/AccordionItem.tsx
+++ packages/react-magma-dom/src/components/Accordion/AccordionItem.tsx
@@ -23,12 +23,12 @@ export interface AccordionItemProps
 }
 
 const StyledItem = styled.div`
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
+  & > h1,
+  & > h2,
+  & > h3,
+  & > h4,
+  & > h5,
+  & > h6 {
     background: none;
     color: inherit;
     font: inherit;
