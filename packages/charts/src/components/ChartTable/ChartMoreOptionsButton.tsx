ac0fbc7dc5e18ac55c34594fe998e6f9e1b1cb41
serhiiyanhol-cengage <serhii.yanhol@cengage.com>
2026-08-28 11:46:01 +0300

    fix(Charts): Add focus support for inner chart content. (#2464) (#2682)
    Co-authored-by: Oleh Noskov <oleh.noskov@cengage.com>

--- packages/charts/src/components/ChartTable/ChartMoreOptionsButton.tsx
+++ packages/charts/src/components/ChartTable/ChartMoreOptionsButton.tsx
@@ -41,6 +41,7 @@ export function ChartMoreOptionsButton({
         aria-label={resolvedAriaLabel}
         icon={icon}
         variant={ButtonVariant.link}
+        testId="chart-more-options-button"
       />
       <DropdownContent>{children}</DropdownContent>
     </Dropdown>
