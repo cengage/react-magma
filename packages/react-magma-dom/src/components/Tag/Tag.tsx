3cda2a9360376b0488f40830f4f3fa0566bad8b4
Artem Trusov <artem.trusov@cengage.com>
2026-09-24 12:53:38 +0200

    fix(storybook): address review comments

--- packages/react-magma-dom/src/components/Tag/Tag.tsx
+++ packages/react-magma-dom/src/components/Tag/Tag.tsx
@@ -349,7 +349,8 @@ export const Tag = React.forwardRef<HTMLButtonElement, TagProps>(
 
     const { icon } = props;
 
-    const StyledTag = getStyledTag(Boolean(onClick || onDelete));
+    const isInteractive = Boolean(onClick || onDelete);
+    const StyledTag = getStyledTag(isInteractive);
 
     function handleClick() {
       if (onClick && typeof onClick === 'function') {
@@ -361,7 +362,9 @@ export const Tag = React.forwardRef<HTMLButtonElement, TagProps>(
 
     return (
       <StyledTag
-        aria-label={onDelete ? deleteAriaLabel : nodeLabel}
+        {...(isInteractive && {
+          'aria-label': onDelete ? deleteAriaLabel : nodeLabel,
+        })}
         color={color}
         icon={icon}
         onClick={handleClick}
